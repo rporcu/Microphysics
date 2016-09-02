466266616997e95478bdb0555aab2965361b1633
Jordan Musser <jordan.musser@netl.doe.gov>
2016-09-02 11:06:56 -0400

    Finish FLD01 case files

--- tests/FLD01/runtests.sh
+++ tests/FLD01/runtests.sh
@@ -3,7 +3,16 @@
 # set case directory
 RUN_NAME="FLD01"
 
-for GMAX in 16 32 64 128; do
+rm -f POST_* &> /dev/null
+
+for GMAX in 8 16 32 64; do
    rm -f ${RUN_NAME}* &> /dev/null
    time -p ./mfix IMAX=${GMAX} JMAX=${GMAX}
+done
+
+post_dats=AUTOTEST/POST*.dat
+
+for test_post_file in ${post_dats}; do
+    numdiff -a 0.000001 -r 0.05 ${test_post_file} \
+      $(basename ${test_post_file})
 done
