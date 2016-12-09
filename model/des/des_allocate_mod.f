ba587d6dc17ec6c4ca9d72ae72f4d78de07a3e4a
Mark Meredith <mwm126@gmail.com>
2016-12-09 11:04:31 -0500

    fixes for commit 0655911b732624cf6e876fcc29b01a1674e812f3.

--- model/des/des_allocate_mod.f
+++ model/des/des_allocate_mod.f
@@ -160,82 +160,6 @@ CONTAINS
    CONTAINS
 
 
-!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
-!                                                                         !
-!  Subrourtine: DES_INIT_PARTICLE_ARRAYS                                  !
-!  Author: Jay Boyalakuntla                              Date: 12-Jun-04  !
-!                                                                         !
-!  Purpose: Initialize particle arrays. The upper and lower bounds are    !
-!  passed so that after resizing particle arrays (see GROW_PARTICLE) the  !
-!  new portions of the arrays can be initialized.                         !
-!                                                                         !
-!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
-      SUBROUTINE DES_INIT_PARTICLE_ARRAYS(LB,UB)
-
-         use discretelement, only: des_radius, ro_sol, pmass, omoi, des_pos_new, des_vel_new, omega_new, particle_state, pvol
-         use discretelement, only: dg_pijk, dg_pijkprv, ighost_updated, neighbor_index, fc, tow, wall_collision_facet_id, pijk
-         use discretelement, only: rot_acc_old, des_usr_var_size
-         use discretelement, only: wall_collision_pft, iglobal_id, drag_fc, des_acc_old, nonexistent, do_old, des_usr_var
-         use param1, only: zero
-
-      IMPLICIT NONE
-
-      INTEGER, INTENT(IN) :: LB, UB
-      INTEGER :: II
-
-      IGLOBAL_ID(LB:UB) = 0
-      PARTICLE_STATE(LB:UB) = NONEXISTENT
-
-! Physical properties:
-      DES_RADIUS(LB:UB) = ZERO
-      RO_Sol(LB:UB) = ZERO
-      PVOL(LB:UB) = ZERO
-      PMASS(LB:UB) = ZERO
-      OMOI(LB:UB) = ZERO
-
-! Particle position, velocity, etc
-      DES_POS_NEW(LB:UB,:) = ZERO
-      DES_VEL_NEW(LB:UB,:) = ZERO
-      OMEGA_NEW(LB:UB,:) = ZERO
-
-! Particle state flag
-      DO II = LB, UB
-         particle_state(II) = nonexistent
-      ENDDO
-      NEIGHBOR_INDEX(:) = 0
-
-! DES grid bin information
-      DG_PIJK(LB:UB) = -1
-      DG_PIJKPRV(LB:UB) = -1
-      IGHOST_UPDATED(LB:UB) = .false.
-
-! Fluid cell bin information
-      PIJK(LB:UB,:) = 0
-
-! Translation and rotational forces
-      FC(LB:UB,:) = ZERO
-      TOW(LB:UB,:) = ZERO
-
-! Collision data
-      WALL_COLLISION_FACET_ID(:,LB:UB) = -1
-      WALL_COLLISION_PFT(:,:,LB:UB) = ZERO
-
-! Initializing user defined array
-      IF(DES_USR_VAR_SIZE > 0) &
-         DES_USR_VAR(LB:UB,:) = ZERO
-
-! Particle center drag coefficient and explicit drag force
-      DRAG_FC(LB:UB,:) = ZERO
-
-! Higher order time integration variables.
-      IF (DO_OLD) THEN
-         DES_ACC_OLD(LB:UB,:) = ZERO
-         ROT_ACC_OLD(LB:UB,:) = ZERO
-      ENDIF
-
-      RETURN
-      END SUBROUTINE DES_INIT_PARTICLE_ARRAYS
-
 
       END SUBROUTINE DES_ALLOCATE_ARRAYS
 
@@ -404,9 +328,89 @@ CONTAINS
            IF(DES_USR_VAR_SIZE > 0) &
               call real_grow2_reverse(DES_USR_VAR,MAX_PIP)
 
+           CALL DES_INIT_PARTICLE_ARRAYS(MAX_PIP/2+1,MAX_PIP)
+
         ENDDO
 
       RETURN
+
+      CONTAINS
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                         !
+!  Subrourtine: DES_INIT_PARTICLE_ARRAYS                                  !
+!  Author: Jay Boyalakuntla                              Date: 12-Jun-04  !
+!                                                                         !
+!  Purpose: Initialize particle arrays. The upper and lower bounds are    !
+!  passed so that after resizing particle arrays (see GROW_PARTICLE) the  !
+!  new portions of the arrays can be initialized.                         !
+!                                                                         !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+      SUBROUTINE DES_INIT_PARTICLE_ARRAYS(LB,UB)
+
+         use discretelement, only: des_radius, ro_sol, pmass, omoi, des_pos_new, des_vel_new, omega_new, particle_state, pvol
+         use discretelement, only: dg_pijk, dg_pijkprv, ighost_updated, neighbor_index, fc, tow, wall_collision_facet_id, pijk
+         use discretelement, only: rot_acc_old, des_usr_var_size
+         use discretelement, only: wall_collision_pft, iglobal_id, drag_fc, des_acc_old, nonexistent, do_old, des_usr_var
+         use param1, only: zero
+
+      IMPLICIT NONE
+
+      INTEGER, INTENT(IN) :: LB, UB
+      INTEGER :: II
+
+      IGLOBAL_ID(LB:UB) = 0
+      PARTICLE_STATE(LB:UB) = NONEXISTENT
+
+! Physical properties:
+      DES_RADIUS(LB:UB) = ZERO
+      RO_Sol(LB:UB) = ZERO
+      PVOL(LB:UB) = ZERO
+      PMASS(LB:UB) = ZERO
+      OMOI(LB:UB) = ZERO
+
+! Particle position, velocity, etc
+      DES_POS_NEW(LB:UB,:) = ZERO
+      DES_VEL_NEW(LB:UB,:) = ZERO
+      OMEGA_NEW(LB:UB,:) = ZERO
+
+! Particle state flag
+      DO II = LB, UB
+         particle_state(II) = nonexistent
+      ENDDO
+      NEIGHBOR_INDEX(:) = 0
+
+! DES grid bin information
+      DG_PIJK(LB:UB) = -1
+      DG_PIJKPRV(LB:UB) = -1
+      IGHOST_UPDATED(LB:UB) = .false.
+
+! Fluid cell bin information
+      PIJK(LB:UB,:) = 0
+
+! Translation and rotational forces
+      FC(LB:UB,:) = ZERO
+      TOW(LB:UB,:) = ZERO
+
+! Collision data
+      WALL_COLLISION_FACET_ID(:,LB:UB) = -1
+      WALL_COLLISION_PFT(:,:,LB:UB) = ZERO
+
+! Initializing user defined array
+      IF(DES_USR_VAR_SIZE > 0) &
+         DES_USR_VAR(LB:UB,:) = ZERO
+
+! Particle center drag coefficient and explicit drag force
+      DRAG_FC(LB:UB,:) = ZERO
+
+! Higher order time integration variables.
+      IF (DO_OLD) THEN
+         DES_ACC_OLD(LB:UB,:) = ZERO
+         ROT_ACC_OLD(LB:UB,:) = ZERO
+      ENDIF
+
+      RETURN
+      END SUBROUTINE DES_INIT_PARTICLE_ARRAYS
+
 
       END SUBROUTINE PARTICLE_GROW
 
--- model/mfix.f
+++ model/mfix.f
@@ -88,7 +88,12 @@
       use set_bc_dem_module, only: set_bc_dem
       use time_march_module, only: time_march
       use zero_norm_vel_module, only: zero_norm_vel
-      use discretelement, only: pinc, des_rop_s
+      use discretelement, only: pinc, des_rop_s, max_pip
+
+      use discretelement, only: des_radius, ro_sol, pmass, omoi, des_pos_new, des_vel_new, omega_new, particle_state, pvol
+      use discretelement, only: dg_pijk, dg_pijkprv, ighost_updated, neighbor_index, fc, tow, wall_collision_facet_id, pijk
+      use discretelement, only: rot_acc_old, des_usr_var_size
+      use discretelement, only: wall_collision_pft, iglobal_id, drag_fc, des_acc_old, nonexistent, do_old, des_usr_var
 
       IMPLICIT NONE
 
@@ -149,6 +154,8 @@
 ! Temporary storage for DT
       DOUBLE PRECISION :: DT_tmp
 
+      INTEGER :: II, lb, ub
+
 
 ! Invoke MPI initialization routines and get rank info.
       ! CALL PARALLEL_INIT
@@ -172,6 +179,59 @@
       IF (DEM_SOLIDS) THEN
          PINC(:,:,:) = 0
          DES_ROP_S(:,:,:,:) = ZERO
+
+         lb = 1
+         ub = MAX_PIP
+
+         IGLOBAL_ID(LB:UB) = 0
+         PARTICLE_STATE(LB:UB) = NONEXISTENT
+
+! Physical properties:
+         DES_RADIUS(LB:UB) = ZERO
+         RO_Sol(LB:UB) = ZERO
+         PVOL(LB:UB) = ZERO
+         PMASS(LB:UB) = ZERO
+         OMOI(LB:UB) = ZERO
+
+! Particle position, velocity, etc
+         DES_POS_NEW(LB:UB,:) = ZERO
+         DES_VEL_NEW(LB:UB,:) = ZERO
+         OMEGA_NEW(LB:UB,:) = ZERO
+
+! Particle state flag
+         DO II = LB, UB
+            particle_state(II) = nonexistent
+         ENDDO
+         NEIGHBOR_INDEX(:) = 0
+
+! DES grid bin information
+         DG_PIJK(LB:UB) = -1
+         DG_PIJKPRV(LB:UB) = -1
+         IGHOST_UPDATED(LB:UB) = .false.
+
+! Fluid cell bin information
+         PIJK(LB:UB,:) = 0
+
+! Translation and rotational forces
+         FC(LB:UB,:) = ZERO
+         TOW(LB:UB,:) = ZERO
+
+! Collision data
+         WALL_COLLISION_FACET_ID(:,LB:UB) = -1
+         WALL_COLLISION_PFT(:,:,LB:UB) = ZERO
+
+! Initializing user defined array
+         IF(DES_USR_VAR_SIZE > 0) &
+            DES_USR_VAR(LB:UB,:) = ZERO
+
+! Particle center drag coefficient and explicit drag force
+         DRAG_FC(LB:UB,:) = ZERO
+
+! Higher order time integration variables.
+         IF (DO_OLD) THEN
+            DES_ACC_OLD(LB:UB,:) = ZERO
+            ROT_ACC_OLD(LB:UB,:) = ZERO
+         ENDIF
       ENDIF
 
 ! Write the initial part of the standard output file
