fc8bfbfc05ff8dbfeda8fc6aa35550b32392b272
Ann Almgren <asalmgren@lbl.gov>
2017-01-15 09:47:38 -0800

    more index changes...passes all tests

--- model/set_flags.f90
+++ model/set_flags.f90
@@ -1,15 +1,17 @@
-MODULE SET_FLAGS_MODULE
-   CONTAINS
+module set_flags_module
+
+contains
+
 !vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
 !                                                                      C
-!  Subroutine: SET_FLAGS                                               C
+!  Subroutine: SET_flagS                                               C
 !  Author: M. Syamlal                                 Date: 29-JAN-92  C
 !                                                                      C
 !  Purpose: This module assigns a flag to a cell to identify its type. C
 !                                                                      C
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
 
-      SUBROUTINE SET_FLAGS(slo,shi,flag)
+      subroutine set_flags(slo,shi,flag)
 
       use ic, only: PINF_, POUT_, MINF_, MOUT_, OUTF_, NSW_, FSW_
       use ic, only: NSW_
@@ -31,7 +33,7 @@ MODULE SET_FLAGS_MODULE
 !-----------------------------------------------
 
 !  Cell flag definitions
-!  FLAG1   FLAG0   BC_TYPE        Cell type
+!  flag1   flag0   BC_TYPE        Cell type
 !  ----- --------- -------        ---------
 !   1       .        -            Cell containing gas or solids or both
 !  10       p      P_INFLOW       Specified pressure inflow cell
@@ -110,11 +112,11 @@ MODULE SET_FLAGS_MODULE
 ! ---------------------------------------------------------------->>>
       flag(:,:,:,2:4) = UNDEFINED_I
 
-      END SUBROUTINE SET_FLAGS
+      end subroutine set_flags
 
 !vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
 !                                                                      C
-!  Module name: SET_FLAGS1                                             C
+!  Module name: SET_flagS1                                             C
 !  Purpose: Assign IP flag to the faces of wall cells                  C
 !                                                                      C
 !  Notes: This routine may still leave flag_e, flag_n and flag_t       C
@@ -126,18 +128,15 @@ MODULE SET_FLAGS_MODULE
 !  Literature/Document References:                                     C
 !                                                                      C
 !  Variables referenced:                                               C
-!  Variables modified: FLAG_E, FLAG_N, FLAG_T                          C
+!  Variables modified: flag_E, flag_N, flag_T                          C
 !  Local variables:                                                    C
 !                                                                      C
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
 
-      SUBROUTINE SET_FLAGS1(slo,shi,flag)
+      subroutine set_flags1(slo,shi,flag)
 
-!-----------------------------------------------
-! Modules
-!-----------------------------------------------
       USE param1   , only: is_undefined
-      USE geometry , only: domhi
+      USE geometry , only: domlo,domhi
       USE functions, only: iminus, iplus, jminus, jplus, kminus, kplus
 
       implicit none
@@ -147,12 +146,7 @@ MODULE SET_FLAGS_MODULE
       integer, intent(inout) :: flag&
          (slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),4)
 
-!-----------------------------------------------
-! Local variables
-!-----------------------------------------------
-! Indices
-      INTEGER :: i,j,k
-!-----------------------------------------------
+      integer :: i,j,k
 
       do k = slo(3),shi(3)
          do j = slo(2),shi(2)
@@ -160,74 +154,74 @@ MODULE SET_FLAGS_MODULE
 
 ! If the flag is greater than or equal to 2000, there is no
 ! internal surface.
-         IF (flag(i,j,k,1)>=100) THEN
+         IF (flag(i,j,k,1)>=100) then
 
 ! ---------------------------------------------------------------->>>
 ! the default is equivalent to an impermeable surface and these cells
 ! will be treated as such in the momentum routines
-            FLAG(i,j,k,2) = 0
-            FLAG(i,j,k,3) = 0
-            FLAG(i,j,k,4) = 0
-            FLAG(iminus(i,j,k),j,k,2) = 0
-            FLAG(i,jminus(i,j,k),k,3) = 0
-            FLAG(i,j,kminus(i,j,k),4) = 0
+            flag(i,j,k,2) = 0
+            flag(i,j,k,3) = 0
+            flag(i,j,k,4) = 0
+            flag(iminus(i,j,k),j,k,2) = 0
+            flag(i,jminus(i,j,k),k,3) = 0
+            flag(i,j,kminus(i,j,k),4) = 0
 
             if(flag(i,j,k,1) == 106 .or. flag(i,j,k,1) == 107) then
+
                ! make the upper (E, N, T) boundary permeable
-               IF (I == domhi(1)+1) THEN
-                  IF ((J/=1.AND.J/=0) .AND. (J/=domhi(2)+1)) THEN
-                     IF ((K/=1.AND.K/=0) .AND. (K/=domhi(3)+1)) THEN
-                        IF(flag(iminus(i,j,k),j,k,1) < 100) &
-                           FLAG(iminus(i,j,k),j,k,2) = 2000
-                     ENDIF
-                  ENDIF
-               ENDIF
-               IF (J == DOMHI(2)+1) THEN
-                  IF ((I/=1.AND.I/=0) .AND. (I/=DOMHI(1)+1.AND.I/=DOMHI(1)+1)) THEN
-                     IF ((K/=1.AND.K/=0) .AND. (K/=DOMHI(3)+1)) THEN
-                        IF(flag(i,jminus(i,j,k),k,1)<100) &
-                           FLAG(i,jminus(i,j,k),k,3) = 2000
-                     ENDIF
-                  ENDIF
-                ENDIF
-               IF (K == DOMHI(3)+1) THEN
-                  IF ((J/=1.AND.J/=0) .AND. (J/=DOMHI(2)+1)) THEN
-                     IF ((I/=1.AND.I/=0) .AND. (I/=DOMHI(1)+1) .AND. &
-                        flag(i,j,kminus(i,j,k),1)<100) &
-                        FLAG(i,j,kminus(i,j,k),4) = 2000
-                  ENDIF
-               ENDIF
+               if (I == domhi(1)+1) then
+                  if ( (J>=(domlo(2)-1)) .and. (J<=(domhi(2)+1)) .and. &
+                       (K>=(domlo(3)-1)) .and. (K<=(domhi(3)+1)) .and. &
+                       (flag(iminus(i,j,k),j,k,1) < 100) ) then
+                           flag(iminus(i,j,k),j,k,2) = 2000
+                  end if
+               end if
+
+               if (J == domhi(2)+1) then
+                  if ( (I>=(domlo(1)-1)) .and. (I<=(domhi(1)+1)) .and. &
+                       (K>=(domlo(3)-1)) .and. (K<=(domhi(3)+1)) .and. &
+                       (flag(i,jminus(i,j,k),k,1) < 100) ) then
+                           flag(i,jminus(i,j,k),k,3) = 2000
+                  end if
+               end if
+
+               if (K == domhi(3)+1) then
+                  if ( (J>=(domlo(2)-1)) .and. (J<=(domhi(2)+1)) .and. &
+                       (I>=(domlo(1)-1)) .and. (I<=(domhi(1)+1)) .and. &
+                       (flag(i,j,kminus(i,j,k),1) < 100) ) then
+                        flag(i,j,kminus(i,j,k),4) = 2000
+                  end if
+               end if
 
             ENDIF
 
 ! ----------------------------------------------------------------<<<
-         ELSEIF (flag(i,j,k,1)==1) THEN
+         ELSEIF (flag(i,j,k,1)==1) then
 ! ---------------------------------------------------------------->>>
 
-            IF ( flag(iminus(i,j,k),j,k,1) < 100 .AND. &
-               IS_UNDEFINED(FLAG(iminus(i,j,k),j,k,2))) &
-               FLAG(iminus(i,j,k),j,k,2) = 2000 + FLAG(iminus(i,j,k),j,k,1)
+            IF ( flag(iminus(i,j,k),j,k,1) < 100 .and. &
+               IS_UNDEFINED(flag(iminus(i,j,k),j,k,2))) &
+               flag(iminus(i,j,k),j,k,2) = 2000 + flag(iminus(i,j,k),j,k,1)
 
-            IF ( flag(iplus(i,j,k),j,k,1) < 100 .AND. &
-               IS_UNDEFINED(FLAG(i,j,k,2))) &
-               FLAG(i,j,k,2) = 2000 + FLAG(iplus(i,j,k),j,k,1)
+            IF ( flag(iplus(i,j,k),j,k,1) < 100 .and. &
+               IS_UNDEFINED(flag(i,j,k,2))) &
+               flag(i,j,k,2) = 2000 + flag(iplus(i,j,k),j,k,1)
 
-            IF ( flag(i,jminus(i,j,k),k,1) < 100 .AND. &
-               IS_UNDEFINED(FLAG(i,jminus(i,j,k),k,3))) &
-               FLAG(i,jminus(i,j,k),k,3) = 2000 + FLAG(i,jminus(i,j,k),k,1)
+            IF ( flag(i,jminus(i,j,k),k,1) < 100 .and. &
+               IS_UNDEFINED(flag(i,jminus(i,j,k),k,3))) &
+               flag(i,jminus(i,j,k),k,3) = 2000 + flag(i,jminus(i,j,k),k,1)
 
-            IF ( flag(i,jplus(i,j,k),k,1) < 100 .AND. &
-               IS_UNDEFINED(FLAG(i,j,k,3))) &
-               FLAG(i,j,k,3) = 2000 + FLAG(i,jplus(i,j,k),k,1)
+            IF ( flag(i,jplus(i,j,k),k,1) < 100 .and. &
+               IS_UNDEFINED(flag(i,j,k,3))) &
+               flag(i,j,k,3) = 2000 + flag(i,jplus(i,j,k),k,1)
 
-            IF ( flag(i,j,kminus(i,j,k),1) < 100 .AND. &
-               IS_UNDEFINED(FLAG(i,j,kminus(i,j,k),4))) &
-               FLAG(i,j,kminus(i,j,k),4) = 2000 + FLAG(i,j,kminus(i,j,k),1)
+            IF ( flag(i,j,kminus(i,j,k),1) < 100 .and. &
+               IS_UNDEFINED(flag(i,j,kminus(i,j,k),4))) &
+               flag(i,j,kminus(i,j,k),4) = 2000 + flag(i,j,kminus(i,j,k),1)
 
-            IF ( flag(i,j,kplus(i,j,k),1) < 100 .AND. &
-               IS_UNDEFINED(FLAG(i,j,k,4))) &
-               FLAG(i,j,k,4) = 2000 + FLAG(i,j,kplus(i,j,k),1)
-
+            IF ( flag(i,j,kplus(i,j,k),1) < 100 .and. &
+               IS_UNDEFINED(flag(i,j,k,4))) &
+               flag(i,j,k,4) = 2000 + flag(i,j,kplus(i,j,k),1)
 
          ENDIF
 
@@ -264,6 +258,6 @@ MODULE SET_FLAGS_MODULE
       ! call send_recv(flag_n,1)
       ! call send_recv(flag_e,1)
 
-      RETURN
-      END SUBROUTINE SET_FLAGS1
-END MODULE SET_FLAGS_MODULE
+      end subroutine set_flags1
+
+end module set_flags_module
