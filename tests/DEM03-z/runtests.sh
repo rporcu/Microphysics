c95794c99fb5043162eaec97c6ea0202c75fd28a
Ann Almgren <asalmgren@lbl.gov>
2017-02-05 14:50:05 -0800

    Add DEM03-z -- thought I had committed this before -- oops.

---
+++ tests/DEM03-z/runtests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -lex
+
+RUN_NAME="DEM03"
+
+MFIX=./mfix
+if [ -n "$1" ]; then
+    MFIX=$1
+fi
+
+rm -f POST_* &> /dev/null
+
+DES_IM=ADAMS_BASHFORTH
+for DES_ETA in 1.0; do
+  rm -f ${RUN_NAME}* &> /dev/null
+  time -p ${MFIX} inputs DES_INTG_METHOD=\"${DES_IM}\" \
+    DES_EN_INPUT\(1\)=${DES_ETA} \
+    DES_EN_INPUT\(2\)=${DES_ETA} \
+    DES_EN_INPUT\(3\)=${DES_ETA} \
+    DES_EN_WALL_INPUT\(1\)=${DES_ETA} \
+    DES_EN_WALL_INPUT\(2\)=${DES_ETA}
+done
+
---
+++ tests/DEM03-z/usr0.f90
@@ -0,0 +1,30 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Module name: USR0                                                   !
+!  Author: J.Musser                                   Date: dd-mmm-yy  !
+!  Purpose: This routine is called before the time loop starts and is  !
+!           user-definable.  The user may insert code in this routine  !
+!           or call appropriate user defined subroutines.  This        !
+!           can be used for setting constants and checking errors in   !
+!           data.  This routine is not called from an IJK loop, hence  !
+!           all indices are undefined.                                 !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
+      subroutine usr0
+
+      use discretelement, only: particles
+      use usr, only: f1b, f2b, gy1, gy2, gz1, gz2
+
+      IMPLICIT NONE
+
+      gz1 = 0.00045d0
+      gz2 = 0.00135d0
+
+      gy1 = 0.0d0
+      gy2 = 0.0d0
+
+      ! Body forces -- zero in this case.
+      F1b = 0.d0
+      F2b = 0.d0
+
+      end subroutine usr0
