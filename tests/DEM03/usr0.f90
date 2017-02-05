3bb521801e2b6a3f534130e0f23b51c25fce4cb7
Ann Almgren <asalmgren@lbl.gov>
2017-02-05 13:01:08 -0800

    I think this is right (DEM03/usr0.f90)..

--- tests/DEM03/usr0.f90
+++ tests/DEM03/usr0.f90
@@ -10,28 +10,21 @@
 !           all indices are undefined.                                 !
 !                                                                      !
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
-      SUBROUTINE USR0
+      subroutine usr0
 
       use discretelement, only: particles
-      use constant, only: gravity
-      use exit_mod, only: mfix_exit
-      use usr, only: f1b, f2b, gx1, gy1, gy2
+      use usr, only: f1b, f2b, gx1, gx2, gy1, gy2
 
       IMPLICIT NONE
 
-      if(particles /= 2) then
-         write(*,"(3x, 'invalid setup for test case')")
-         call mfix_exit(0)
-      endif
-
       gY1 = 0.00045d0
       gY2 = 0.00135d0
-      gX1 = 0.0d0
-      gX1 = 0.0d0
 
-! Body forces. (Gravity)
-      F1b = GRAVITY(2)
-      F2b = GRAVITY(2)
+      gX1 = 0.0d0
+      gX2 = 0.0d0
 
-      return
-      END SUBROUTINE USR0
+      ! Body forces -- zero in this case.
+      F1b = 0.d0
+      F2b = 0.d0
+
+      end subroutine usr0
