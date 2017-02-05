680fcfd62db090e75ecce16d71600385a2bc7d3f
Ann Almgren <asalmgren@lbl.gov>
2017-02-05 08:12:16 -0800

    Don't need the first function in the file: vavg_g itself

--- model/vavg_g.f90
+++ model/vavg_g.f90
@@ -26,62 +26,6 @@ MODULE VAVG_MOD
 !                                                                      C
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
 !
-      real(c_real) function vavg_g (slo, shi, vel_g, ep_g, vol, flag)
-
-!-----------------------------------------------
-!   M o d u l e s
-!-----------------------------------------------
-      USE param1   , ONLY: ZERO
-
-      IMPLICIT NONE
-
-      integer     , intent(in   ) :: slo(3),shi(3)
-
-      ! Cell volume
-      real(c_real), INTENT(IN   ) :: VOL
-
-      ! component of gas velocity
-      real(c_real), INTENT(IN   ) :: vel_g&
-         (slo(1):shi(1),slo(2):shi(2),slo(3):shi(3))
-
-      real(c_real), INTENT(IN   ) :: ep_g&
-         (slo(1):shi(1),slo(2):shi(2),slo(3):shi(3))
-
-      integer, intent(in   ) :: flag &
-         (slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),4)
-
-!     Integral of U_g*EP_g for entire volume
-      real(c_real) :: sum_g
-
-!                      Indices
-      INTEGER :: I,J,K
-
-!                      Total volume of computational cells
-      real(c_real) SUM_VOL
-
-!  Integrate the velocity values for the whole domain,
-!
-      SUM_G = ZERO
-      SUM_VOL = ZERO
-
-      DO K = slo(3),shi(3)
-      DO J = slo(2),shi(2)
-      DO I = slo(1),shi(1)
-         IF (1.eq.flag(i,j,k,1)) THEN
-            SUM_VOL = SUM_VOL + VOL
-            SUM_G = SUM_G + vel_G(I,J,K)*EP_G(I,J,K)*VOL
-         ENDIF
-      END DO
-      END DO
-      END DO
-
-      ! CALL GLOBAL_ALL_SUM(SUM_VOL)
-      ! CALL GLOBAL_ALL_SUM(SUM_G)
-
-      VAVG_G = SUM_G/SUM_VOL
-
-      end function vavg_g
-
       real(c_real) function vavg_flux_g (slo, shi, FLUX_G, A_FACE, flag)
 
       USE param1, only: zero
@@ -122,9 +66,6 @@ MODULE VAVG_MOD
       END DO
       END DO
       END DO
-
-      ! CALL GLOBAL_ALL_SUM(SUM_AREA)
-      ! CALL GLOBAL_ALL_SUM(SUM_G)
 
       vavg_flux_g = SUM_G/SUM_AREA
 
