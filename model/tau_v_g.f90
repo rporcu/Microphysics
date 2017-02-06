617ac5e6ec67869fe94dd7a24aee04a2b3ae4a22
Ann Almgren <asalmgren@lbl.gov>
2017-02-05 21:01:38 -0800

    Remove more ieast, etc from tau_v_g.f90

--- model/tau_v_g.f90
+++ model/tau_v_g.f90
@@ -1,4 +1,4 @@
-MODULE CALC_TAU_V_G_MODULE
+module calc_tau_v_g_module
 
    use bl_fort_module, only : c_real
    use iso_c_binding , only: c_int
@@ -40,11 +40,11 @@ MODULE CALC_TAU_V_G_MODULE
 !  (i.e., those of the form mu.grad(v)                                 C
 !                                                                      C
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
-      SUBROUTINE CALC_TAU_V_G(slo,shi,lo,hi,&
-                              lTAU_V_G,trd_g,ep_g,u_g,v_g,w_g,lambda_g,mu_g,flag,dx,dy,dz)
+      subroutine calc_tau_v_g(slo,shi,lo,hi,&
+                              lTAU_v_g,trd_g,ep_g,u_g,v_g,w_g,lambda_g,mu_g,flag,dx,dy,dz)
 
-      USE param1, only: zero
-      USE toleranc, only: dil_ep_s
+      use functions, only: avg, avg_h 
+      use toleranc , only: dil_ep_s
       IMPLICIT NONE
 
       integer     , intent(in   ) :: slo(3),shi(3),lo(3),hi(3)
@@ -74,7 +74,7 @@ MODULE CALC_TAU_V_G_MODULE
 ! Local variables
 !---------------------------------------------------------------------//
 ! Indices
-      INTEGER :: I, J, K, IM, JP, KM
+      INTEGER :: i, j, k, im, jp, km
 ! Average volume fraction
       real(c_real) :: EPGA
 ! Source terms (Surface)
@@ -92,77 +92,73 @@ MODULE CALC_TAU_V_G_MODULE
 !             use of the *tmp variables below
 !---------------------------------------------------------------------//
 
-        DO K = lo(3),hi(3)
-         DO J = lo(2),hi(2)-1
-          DO I = lo(1),hi(1)
+        DO k = lo(3),hi(3)
+         DO j = lo(2),hi(2)-1
+          DO i = lo(1),hi(1)
 
-            EPGA = AVG(EP_G(I,J,K),EP_G(i,jnorth(i,j,k),k))
+            EPGA = AVG(EP_G(i,j,k),EP_G(i,j+1,k))
             IF (flag(i,j,k,3) > 1000 .AND. EPGA>DIL_EP_S) THEN
 
-               IM = max(domlo(1)-1, i-1)
-               JP = min(domhi(2)+1, j+1)
-               KM = max(domlo(3)-1, k-1)
+               im = max(domlo(1)-1, i-1)
+               jp = min(domhi(2)+1, j+1)
+               km = max(domlo(3)-1, k-1)
 
 ! Surface forces at i, j+1/2, k
 ! bulk viscosity term
 ! part of d/dy (tau_yy) xdxdydz =>
 !         d/dy (lambda.trcD) xdxdydz =>
 ! delta (lambda.trcD)Ap|N-S  : at (i, j+1 - j-1, k)
-               SBV = (LAMBDA_G(i,jnorth(i,j,k),k)*TRD_G(i,jnorth(i,j,k),k)-&
-                      LAMBDA_G(i,j,k)*TRD_G(i,j,k))*AXZ
+               SBV = (LAMBDA_G(i,j+1,k)*TRD_G(i,j+1,k)-&
+                      LAMBDA_G(i,j  ,k)*TRD_G(i,j  ,k))*AXZ
 
 ! shear stress terms at i, j+1/2, k
 
 ! part of 1/x d/dx(x.tau_xy) xdxdydz =>
 !         1/x d/dx (x.mu.du/dy) xdxdydz =>
 ! delta (x.mu.du/dy)Ayz |E-W : at (i+1/2 - i-1/2, j+1/2, k)
-               SSX = AVG_H(AVG_H(MU_G(i,j,k), &
-                                 MU_G(ieast(i,j,k),j,k)),&
-                           AVG_H(MU_G(i,jnorth(i,j,k),k), &
-                                 MU_G(ieast(i,jnorth(i,j,k),k),jnorth(i,j,k),k))) &
-                    *(U_G(I,JPlus(i,j,k),K)-U_G(I,J,K))*ODY*AYZ &
-                    - AVG_H(AVG_H(MU_G(i-1,j,k), &
-                                  MU_G(i,j,k)), &
-                            AVG_H(MU_G(i-1,jnorth(i-1,j,k),k), &
-                                  MU_G(i,jnorth(i,j,k),k))) &
-                    *(U_G(IMinus(i,j,k),JPlus(i,j,k),K)-U_G(IMinus(i,j,k),J,K))*ODY*AYZ
+               SSX = AVG_H(AVG_H(mu_g(i  ,j  ,k), &
+                                 mu_g(i+1,j  ,k)),&
+                           AVG_H(mu_g(i  ,j+1,k), &
+                                 mu_g(i+1,j+1,k))) &
+                    *(u_g(i,j+1,k)-u_g(i,j,k))*ODY*AYZ &
+                    - AVG_H(AVG_H(mu_g(i-1,j  ,k), &
+                                  mu_g(i  ,j  ,k)), &
+                            AVG_H(mu_g(i-1,j+1,k), &
+                                  mu_g(i  ,j+1,k))) &
+                    *(u_g(i-1,j+1,k)-u_g(i-1,j,k))*ODY*AYZ
 
 ! part of d/dy (tau_xy) xdxdydz =>
 !         d/dy (mu.dv/dy) xdxdydz =>
 ! delta (mu.dv/dx)Axz |N-S : at (i, j+1 - j-1, k)
-               SSY = MU_G(i,jnorth(i,j,k),k)*(V_G(I,JPlus(i,j,k),K)-V_G(I,J,K))*ODY*AXZ - &
-                     MU_G(i,j,k)*(V_G(I,J,K)-V_G(I,JMinus(i,j,k),K))*ODY*AXZ
+               SSY = mu_g(i,j+1,k)*(v_g(i,j+1,k)-v_g(i,j,k))*ODY*AXZ - &
+                     mu_g(i,j,k)*(v_g(i,j,k)-v_g(i,j-1,k))*ODY*AXZ
 
 ! part of 1/x d/dz (tau_xz) xdxdydz =>
 !         1/x d/dz (mu.dw/dy) xdxdydz =>
 ! delta (mu.dw/dx)Axy |T-B : at (i, j+1/2, k+1/2 - k-1/2)
-               SSZ = AVG_H(AVG_H(MU_G(i,j,k), &
-                                 MU_G(i,j,ktop(i,j,k))), &
-                           AVG_H(MU_G(i,jnorth(i,j,k),k), &
-                                 MU_G(i,jnorth(i,j,ktop(i,j,k)),ktop(i,j,k)))) &
-                    *(W_G(I,JPlus(i,j,k),K)-W_G(I,J,K))*ODY*AXY &
-                    - AVG_H(AVG_H(MU_G(i,j,kbot(i,j,k)), &
-                                  MU_G(i,j,k)), &
-                            AVG_H(MU_G(i,jnorth(i,j,kbot(i,j,k)),kbot(i,j,k)), &
-                                  MU_G(i,jnorth(i,j,k),k))) &
-                    *(W_G(I,JPlus(i,j,k),KMinus(i,j,k))-W_G(I,J,KMinus(i,j,k)))*ODY*AXY
+               SSZ = AVG_H(AVG_H(mu_g(i,j  ,k), &
+                                 mu_g(i,j  ,k+1)), &
+                           AVG_H(mu_g(i,j+1,k  ), &
+                                 mu_g(i,j+1,k+1))) &
+                    *(w_g(i,j+1,k)-w_g(i,j,k))*ODY*AXY &
+                    - AVG_H(AVG_H(mu_g(i,j  ,k-1), &
+                                  mu_g(i,j  ,k  )), &
+                            AVG_H(mu_g(i,j+1,k-1), &
+                                  mu_g(i,j+1,k  ))) &
+                    *(w_g(i,j+1,k-1)-w_g(i,j,k-1))*ODY*AXY
 
-! Add the terms
-               lTAU_V_G(i,j,k) = SBV + SSX + SSY + SSZ
+               ! Add the terms
+               lTAU_v_g(i,j,k) = SBV + SSX + SSY + SSZ
 
             ELSE
-               lTAU_V_G(i,j,k) = ZERO
+
+               lTAU_v_g(i,j,k) = 0.d0
+
             ENDIF
          ENDDO
          ENDDO
          ENDDO
 
-      ! call send_recv(ltau_v_g,2)
-      RETURN
+    end subroutine calc_tau_v_g
 
-    CONTAINS
-
-      INCLUDE 'functions.inc'
-
-    END SUBROUTINE CALC_TAU_V_G
-END MODULE CALC_TAU_V_G_MODULE
+end module calc_tau_v_g_module
