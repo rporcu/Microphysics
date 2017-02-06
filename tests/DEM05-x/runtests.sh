58a55f0c300492f96458032387b60d1aa46c08af
Ann Almgren <asalmgren@lbl.gov>
2017-02-06 12:45:44 -0800

    1) Renamed DEM05 --> DEM05-y 2) Added DEM05-x -- it passes!

---
+++ tests/DEM05-x/runtests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -lex
+
+RUN_NAME="DEM05"
+
+MFIX=./mfix
+if [ -n "$1" ]; then
+    MFIX=$1
+fi
+
+rm -f POST_* &> /dev/null
+
+rm -f ${RUN_NAME}* &> /dev/null
+time -p ${MFIX} inputs DES_COLL_MODEL=\"LSD\" \
+  KN=1.72d7 KT_FAC="@(1.48/1.72)" KN_W=1.72d7 KT_W_FAC="@(1.48/1.72)" \
+  "DES_EN_INPUT(1:3)=3*1.0 DES_EN_WALL_INPUT(1:2)=2*1.0"
+
+rm -f ${RUN_NAME}* &> /dev/null
+time -p ${MFIX} inputs DES_COLL_MODEL=\"HERTZIAN\" \
+  "E_YOUNG(1)=380.0d9 E_YOUNG(2)=70.0d9 Ew_YOUNG=70.0d9" \
+  "V_POISSON(1)=0.23  V_POISSON(2)=0.25 Vw_POISSON=0.25" \
+  "DES_EN_INPUT(1:3)=3*1.0 DES_EN_WALL_INPUT(1:2)=2*1.0" \
+  "DES_ET_INPUT(1:3)=3*1.0 DES_ET_WALL_INPUT(1:2)=2*1.0"
+
+post_dats=../DEM05-y/AUTOTEST/POST*.dat
+
+for test_post_file in ${post_dats}; do
+    numdiff -a 0.000001 -r 0.05 ${test_post_file} $(basename ${test_post_file})
+done
---
+++ tests/DEM05-x/usr3_des.f90
@@ -0,0 +1,113 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Module name: URS3_DES                                               !
+!  Author: J.Musser                                   Date: 17-Nov-14  !
+!                                                                      !
+!  REF: Di Renzo, A. and Di Maio F.P. "Comparison of contact-force     !
+!       models for the simulation of collisions in DEM-based granular  !
+!       flow codes," Chemical Engineering Science, 59(3), pg 525-541.  !
+!                                                                      !
+!  REF: Kharaz, A.H., Gorham, D.A., and Salman, A.D. "An experimental  !
+!       study of the elastic rebound of spheres," Powder Technology,   !
+!       120(3), pg 281-291.                                            !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+      SUBROUTINE USR3_DES(max_pip, des_pos_new, des_vel_new, omega_new)
+
+      use compar, only: myPE, PE_IO
+      use constant, only: PI
+
+      use usr, only: init_angle, init_vel_t
+      use bl_fort_module, only : c_real
+
+      IMPLICIT NONE
+
+      integer     , intent(in   ) :: max_pip
+      real(c_real), intent(in   ) :: des_pos_new(max_pip,3)
+      real(c_real), intent(in   ) :: des_vel_new(max_pip,3)
+      real(c_real), intent(in   ) :: omega_new(max_pip,3)
+
+! Local variables
+!---------------------------------------------------------------------//
+! file name
+      CHARACTER(LEN=64) :: FNAME
+! file unit for heat transfer data
+      INTEGER, PARAMETER :: UDF_UNIT = 2030
+! Calculated tangential restitution coefficient.
+      REAL(C_REAL) :: RST_COEFF(2)
+! Rebound Angle (degrees)
+      REAL(C_REAL) :: RBND_ANGLE(2)
+! Particle loop counter
+      INTEGER :: LC, NP
+! YZ Velocity Mag
+      REAL(C_REAL) :: VEL_YZ(2), ROT_YZ(2)
+
+! Particle-Wall Rebound Angle (degrees)
+!---------------------------------------------------------------------//
+! Open the files.
+      FNAME = 'POST_ALPHA.dat'
+      OPEN(UNIT=UDF_UNIT,FILE=FNAME, POSITION="APPEND",STATUS='OLD')
+
+      IF(myPE == PE_IO) THEN
+         DO LC=1,31
+! Calculate the particle-wall rebound angle.
+            NP = LC+31
+            VEL_YZ(1) = sqrt(des_vel_new(NP,2)**2 + des_vel_new(NP,3)**2)
+            RBND_ANGLE(1) = atan(VEL_YZ(1)/des_vel_new(NP,1))*180.0/PI
+
+! Calculate the particle-wall rebound angle.
+            NP = LC
+            VEL_YZ(2) = sqrt(des_vel_new(NP,2)**2 + des_vel_new(NP,3)**2)
+            RBND_ANGLE(2) = atan(VEL_YZ(2)/des_vel_new(NP,1))*180.0/PI
+
+! Write the results to a file.
+            WRITE(UDF_UNIT,"(3(3x,F11.4))") INIT_ANGLE(NP), RBND_ANGLE(1:2)
+         ENDDO
+         CLOSE(UDF_UNIT)
+
+! Particle-wall Tangential Restitution Coefficient
+!---------------------------------------------------------------------//
+! Open the files.
+         FNAME = 'POST_COEFF.dat'
+         OPEN(UNIT=UDF_UNIT,FILE=FNAME, POSITION="APPEND",STATUS='OLD')
+
+         DO LC=2, 31
+! Calculate the particle-wall restitution coefficient.
+            NP=LC+31
+            VEL_YZ(1) = sqrt(des_vel_new(NP,2)**2 + des_vel_new(NP,3)**2)
+            RST_COEFF(1) =  VEL_YZ(1)/INIT_VEL_T(NP)
+! Calculate the particle-wall restitution coefficient.
+            NP=LC
+            VEL_YZ(2) = sqrt(des_vel_new(NP,2)**2 + des_vel_new(NP,3)**2)
+            RST_COEFF(2) =  VEL_YZ(2)/INIT_VEL_T(NP)
+! Write the results to a file.
+            WRITE(UDF_UNIT,"(3(3x,F11.4))") INIT_ANGLE(NP), RST_COEFF(1:2)
+         ENDDO
+         CLOSE(UDF_UNIT)
+      ENDIF
+
+! Particle-wall Angular velocity (rad/sec)
+!---------------------------------------------------------------------//
+! Open the files.
+      IF(myPE == PE_IO) THEN
+         FNAME = 'POST_OMEGA.dat'
+         OPEN(UNIT=UDF_UNIT,FILE=FNAME, POSITION="APPEND",STATUS='OLD')
+
+         DO LC=1,31
+
+! Calculate particle-particle angular velocity
+            NP=LC+31
+            ROT_YZ(1) = sqrt(omega_new(NP,2)**2 + omega_new(NP,3)**2)
+
+! Calculate particle-wall angular velocity
+            NP=LC
+            ROT_YZ(2) = sqrt(omega_new(NP,2)**2 + omega_new(NP,3)**2)
+
+! Write the results to a file.
+            WRITE(UDF_UNIT,"(3(3x,F11.4))") INIT_ANGLE(NP), ROT_YZ(1:2)
+         ENDDO
+         CLOSE(UDF_UNIT)
+      ENDIF
+
+      RETURN
+      END SUBROUTINE USR3_DES
