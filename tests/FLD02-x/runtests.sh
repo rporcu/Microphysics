d67837e95a4939ffa84b60553d96f0524a1671d9
Jordan Musser <jordan.musser@netl.doe.gov>
2017-03-31 08:07:41 -0400

    Changing for new test format

--- tests/FLD02-x/runtests.sh
+++ tests/FLD02-x/runtests.sh
@@ -7,17 +7,19 @@ if [ -n "$1" ]; then
     MFIX=$1
 fi
 
-rm -f POST_* &> /dev/null
+rm -rf POST_* ${RUN_NAME}* &> /dev/null
+time -p ${MFIX} inputs
 
-for DELP_X in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0; do
-  rm -rf ${RUN_NAME}* &> /dev/null
-  time -p ${MFIX} inputs DELP_X=${DELP_X}
-  ${FEXTRACT} -p FLD0200000/ -d 2 -v u_g
-  cat FLD0200000.slice >> POST_UG.dat
-  echo -e "\n\n\n" >> POST_UG.dat
-done
+if ! [ -z "${MFIX_BENCHMARKS_HOME}" ] && ! [ -z "${FCOMPARE}" ]; then
+    ${FCOMPARE} ${MFIX_BENCHMARKS_HOME}/FLD02-x_FLD02-x_plt00000 FLD0200000/
+fi
 
-post_dats=POST*.dat
-for result in ${post_dats}; do
-    numdiff -a 0.0 AUTOTEST/${result} ${result}
-done
+if ! [ -z "${FEXTRACT}" ]; then
+    ${FEXTRACT} -p FLD0100000/ -d 2 -v u_g && mv FLD0100000.slice POST_UG.dat
+    ${FEXTRACT} -p FLD0100000/ -d 1 -v p_g && mv FLD0100000.slice POST_PG.dat
+
+    post_dats=POST*.dat
+    for result in ${post_dats}; do
+        numdiff -a 0.0 AUTOTEST/${result} ${result}
+    done
+fi
