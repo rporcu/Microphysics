558c64c7d626490e07979b03d7745627b6123e7f
Michele Rosso <mrosso@lbl.gov>
2017-04-06 15:35:31 -0700

    Add methods to read in particle info from ASCII file.
    Code provided by Andrew Myers.

--- src/ParticleContainer.cpp
+++ src/ParticleContainer.cpp
@@ -1,6 +1,9 @@
-#include <limits>
-
+#include <AMReX.H>
+#include "AMReX_Particles.H"
+#include <iostream>
 #include <ParticleContainer.H>
+
+using namespace amrex;
 
 int     MFIXParticleContainer::do_tiling = 0;
 IntVect MFIXParticleContainer::tile_size   { D_DECL(1024000,8,8) };
@@ -12,10 +15,74 @@ MFIXParticleContainer::MFIXParticleContainer (AmrCore* amr_core)
     ReadStaticParameters();
 
     this->SetVerbose(0);
-
-    // m_particles.reserve(m_gdb->maxLevel()+1);
-    // m_particles.resize (m_gdb->finestLevel()+1);
 }
+
+
+
+void 
+MFIXParticleContainer::InitParticlesAscii(const std::string& file) {
+        
+    // only read the file on the IO proc
+    if (ParallelDescriptor::MyProc() ==  ParallelDescriptor::IOProcessorNumber()) {
+    	std::ifstream ifs;
+    	ifs.open(file.c_str(), std::ios::in);
+        
+    	if (!ifs.good())
+    	    amrex::FileOpenFailed(file);
+        
+    	int num_particles = 0;
+    	ifs >> num_particles >> std::ws;
+
+    	// we add all the particles to grid 0 and tile 0 and let 
+    	// Redistribute() put them in the right places.
+    	const int lev = 0;
+    	const int grid = 0;
+    	const int tile = 0;
+
+    	ParticleType p;
+    	std::array<Real, PIdx::nattribs> attribs;
+    	for (int i = 0; i < num_particles; i++) {
+     
+    	    // Here we read the struct data into the particle.
+    	    ifs >> p.pos(0);
+    	    ifs >> p.pos(1);
+    	    ifs >> p.pos(2);
+    	    p.id() = ParticleType::NextID();
+    	    p.cpu() = ParallelDescriptor::MyProc();
+                
+    	    // here were read the additional attributes to be stored in SoA style.
+    	    ifs >> attribs[0];
+    	    ifs >> attribs[1];
+    	    ifs >> attribs[2];
+                
+    	    // add them to the data structure
+    	    auto& particle_tile = GetParticles(lev)[std::make_pair(grid,tile)];
+    	    particle_tile.push_back(p);
+    	    particle_tile.push_back(attribs);
+    	}
+    }
+    Redistribute();
+}
+
+
+void
+MFIXParticleContainer:: printParticles() {
+    const int lev = 0;
+    const auto& plevel = GetParticles(lev);
+    for (const auto& kv : plevel) {
+    	const auto& particle_structs = kv.second.GetArrayOfStructs();
+    	const auto& particle_attribs = kv.second.GetStructOfArrays();
+    	for (unsigned i = 0; i < particle_structs.numParticles(); ++i) {
+    	    std::cout << particle_structs[i] << " ";
+    	    for (int j = 0; j < PIdx::nattribs; j++) {
+    		std::cout << particle_attribs[j][i] << " ";
+    	    }
+    	    std::cout << std::endl;
+    	}
+    }
+}
+
+
 
 void
 MFIXParticleContainer::ReadStaticParameters ()
@@ -54,4 +121,7 @@ MFIXParticleContainer::Evolve (int lev, Real dt)
     Real dx[3] = { gm.CellSize(0), std::numeric_limits<Real>::quiet_NaN(), gm.CellSize(1) };
 #endif
 }
+
+
+
 
