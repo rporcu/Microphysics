da0f11a764e166fe4e5b1f85d26477f2d3992b50
Jordan Musser <jordan.musser@netl.doe.gov>
2017-05-19 09:46:52 -0400

    Fail tests when FEXTRACT is not set

--- tests/FLD01-z/runtests-dmp.sh
+++ tests/FLD01-z/runtests-dmp.sh
@@ -12,20 +12,22 @@ if [ -n "$2" ]; then
     FCOMPARE=$2/plt_compare_diff_grids
     FEXTRACT=$2/fextract
 fi
+if [ -z "${FEXTRACT}" ]; then
+    echo "FEXTRACT is not set. Aborting test."
+    exit 1
+fi
 
 rm -rf POST_* ${RUN_NAME}* &> /dev/null
 time -p  mpirun -np 4 ${MFIX} inputs
 
+${FEXTRACT} -p FLD0100000/ -d 1 -v w_g && mv FLD0100000.slice POST_WG_MPI.dat
+${FEXTRACT} -p FLD0100000/ -d 3 -v p_g && mv FLD0100000.slice POST_PG_MPI.dat
+
+post_dats=POST*.dat
+for result in ${post_dats}; do
+  numdiff -a 0.0 -r 0.0 AUTOTEST/${result} ${result}
+done
+
 if ! [ -z "${MFIX_BENCHMARKS_HOME}" ] && ! [ -z "${FCOMPARE}" ]; then
-  ${FCOMPARE} --infile1 ${MFIX_BENCHMARKS_HOME}/FLD01-z_FLD01-z_plt00000 --infile2 FLD0100000/
-fi
-
-if ! [ -z "${FEXTRACT}" ]; then
-  ${FEXTRACT} -p FLD0100000/ -d 1 -v w_g && mv FLD0100000.slice POST_WG_MPI.dat
-  ${FEXTRACT} -p FLD0100000/ -d 3 -v p_g && mv FLD0100000.slice POST_PG_MPI.dat
-
-  post_dats=POST*.dat
-  for result in ${post_dats}; do
-    numdiff -a 0.0 -r 0.0 AUTOTEST/${result} ${result}
-  done
+    ${FCOMPARE} --infile1 ${MFIX_BENCHMARKS_HOME}/FLD01-z_FLD01-z_plt00000 --infile2 FLD0100000/
 fi
