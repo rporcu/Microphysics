eec4535165ba510051611ce705e0633a183bb3d7
Ann Almgren <asalmgren@lbl.gov>
2017-05-22 20:48:42 -0700

    Change tests/DEM06-*/*.f90 to use real(c_real) etc and double precision constants

--- tests/DEM06-z/usr_mod.f90
+++ tests/DEM06-z/usr_mod.f90
@@ -1,21 +1,21 @@
-      MODULE usr
+      module usr
 
-      ! use param
-      ! use param
+      use amrex_fort_module, only : c_real => amrex_real
+      use iso_c_binding , only: c_int
 
       implicit none
 
       ! a dummy variable listed in usrnlst.inc
-      DOUBLE PRECISION DUMMY_DP
+      real(c_real) DUMMY_DP
 
       ! Initial position and velocity
-      DOUBLE PRECISION :: RK4_POS(3)
-      DOUBLE PRECISION :: RK4_VEL(3)
+      real(c_real) :: RK4_POS(3)
+      real(c_real) :: RK4_VEL(3)
 
-      DOUBLE PRECISION :: RK4_TIME = 0.0d0
+      real(c_real) :: RK4_TIME = 0.0d0
 
       ! Fixed gas velocity: (m/sec)
-      DOUBLE PRECISION, PARAMETER :: Vg(3) = (/0.0d0, 0.0d0, 0.4d0/)
+      real(c_real), parameter :: Vg(3) = (/0.0d0, 0.0d0, 0.4d0/)
 
       contains
 
@@ -27,33 +27,35 @@
 !  Purpose: Integrate the RK4 solution from RK4_TIME to T1.            !
 !                                                                      !
 !......................................................................!
-      SUBROUTINE UPDATE_RK4_SOL(T1)
+      subroutine UPDATE_RK4_SOL(T1)
 
-      IMPLICIT NONE
+      implicit none
 
-      DOUBLE PRECISION, INTENT(in) :: T1
+      real(c_real), INTENT(in) :: T1
 
-! Target step size.
-      DOUBLE PRECISION, PARAMETER :: RK4_DT_DEFAULT = 1.0d-8
-! Time step size adjusted for uniform integration.
-      DOUBLE PRECISION :: RK4_DT
-! Number of integration steps.
-      INTEGER :: RK4_STEPS
-! Loop counter.
-      INTEGER :: LC
+      ! Target step size.
+      real(c_real), parameter :: RK4_DT_DEFAULT = 1.0d-8
 
-! Calculate the value for the RK4 solutions.
+      ! Time step size adjusted for uniform integration.
+      real(c_real) :: RK4_DT
+
+      ! Number of integration steps.
+      integer(c_int) :: RK4_STEPS
+
+      ! Loop counter.
+      integer(c_int) :: LC
+
+      ! Calculate the value for the RK4 solutions.
       RK4_STEPS = max(1,ceiling((T1-RK4_TIME)/RK4_DT_DEFAULT))
       RK4_DT = (T1-RK4_TIME)/dble(RK4_STEPS)
 
-! Take the total number of RK4 steps.
-      DO LC=1, RK4_STEPS
+      ! Take the total number of RK4 steps.
+      do LC=1, RK4_STEPS
          CALL RK4_V2b3(RK4_DT, RK4_POS, RK4_VEL)
          RK4_TIME = RK4_TIME + RK4_DT
-      ENDDO
+      end do
 
-      RETURN
-      END SUBROUTINE UPDATE_RK4_SOL
+      end subroutine UPDATE_RK4_SOL
 
 !......................................................................!
 !                                                                      !
@@ -68,17 +70,17 @@
 !  https://mfix.netl.doe.gov/documentation/dem_doc_2012-1.pdf,         !
 !  page 22, Equation (67).                                             !
 !......................................................................!
-      SUBROUTINE RK4_V2b3(DT, POS, VEL)
+      subroutine RK4_V2b3(DT, POS, VEL)
 
       implicit none
 
-      double precision, intent(in) :: DT
+      real(c_real), intent(in) :: DT
 
-      double precision, intent(inout) :: POS(3)
-      double precision, intent(inout) :: VEL(3)
+      real(c_real), intent(inout) :: POS(3)
+      real(c_real), intent(inout) :: VEL(3)
 
-      double precision :: POS_K1(3), POS_K2(3), POS_K3(3), POS_K4(3)
-      double precision :: VEL_K1(3), VEL_K2(3), VEL_K3(3), VEL_K4(3)
+      real(c_real) :: POS_K1(3), POS_K2(3), POS_K3(3), POS_K4(3)
+      real(c_real) :: VEL_K1(3), VEL_K2(3), VEL_K3(3), VEL_K4(3)
 
       POS_K1 = DT * VEL
       VEL_K1 = DT * (Fb() + Fd(VEL))
@@ -95,8 +97,7 @@
       POS = POS + (POS_K1 + 2.0d0*POS_K2 + 2.0d0*POS_K3 + POS_K4)/6.0d0
       VEL = VEL + (VEL_K1 + 2.0d0*VEL_K2 + 2.0d0*VEL_K3 + VEL_K4)/6.0d0
 
-      RETURN
-      END SUBROUTINE RK4_V2b3
+      end subroutine RK4_V2b3
 
 !......................................................................!
 !                                                                      !
@@ -110,19 +111,18 @@
 !  https://mfix.netl.doe.gov/documentation/dem_doc_2012-1.pdf,         !
 !  page 22, Equation (67).                                             !
 !......................................................................!
-      double precision function Re(Vs)
+      real(c_real) function Re(Vs)
 
       use constant, only: D_p0
-      use fld_const, only: RO_G0, MU_g0
+      use fld_const, only: RO_G0, MUug0
 
       implicit none
 
-! Slip velocity magnitude
-      double precision, intent(in) :: Vs
+      ! Slip velocity magnitude
+      real(c_real), intent(in) :: Vs
 
-      Re = (RO_g0 * D_p0(1) * Vs) / MU_g0
+      Re = (RO_g0 * D_p0(1) * Vs) / Mu_g0
 
-      return
       end function Re
 
 !......................................................................!
@@ -135,16 +135,15 @@
 !  Ref: Schiller and Naumman. (1933) 'A drag coefficient correlation', !
 !  Z.Ver. Deutsch Ing., pages 318-320.                                 !
 !......................................................................!
-      double precision function Cd(Re)
+      real(c_real) function Cd(Re)
 
       implicit none
 
-      double precision, intent(in) :: Re  ! particle height.
+      real(c_real), intent(in) :: Re  ! particle height.
 
       Cd = 0.0d0
-      IF(Re > 0.0d0) Cd = (24.0d0/Re)*(1.0d0 + 0.15d0*(Re**0.687))
+      if (Re > 0.0d0) Cd = (24.0d0/Re)*(1.0d0 + 0.15d0*(Re**0.687))
 
-      return
       end function Cd
 
 !......................................................................!
@@ -161,17 +160,15 @@
 !......................................................................!
       function Fb( )
 
-      use constant, only: GRAVITY
+      use constant , only: gravity, RO_s0
       use fld_const, only: RO_g0
-      use constant, only: RO_s0
 
       implicit none
 
-      double precision, dimension(3) :: Fb
+      real(c_real), dimension(3) :: Fb
 
-      Fb(:) = GRAVITY(:) * (RO_s0(1)-RO_g0)/RO_s0(1)
+      Fb(:) = gravity(:) * (RO_s0(1)-RO_g0)/RO_s0(1)
 
-      return
       end function Fb
 
 !......................................................................!
@@ -193,21 +190,20 @@
 
       implicit none
 
-      double precision, intent(in) :: Vp(3) ! particle velocity
+      real(c_real), intent(in) :: Vp(3) ! particle velocity
 
-      double precision, dimension(3) :: Fd
-      double precision, dimension(3) :: Vpg
-      double precision :: Vs
+      real(c_real) :: Fd(3)
+      real(c_real) :: Vpg(3)
+      real(c_real) :: Vs
 
       Vpg(1) = (Vp(1) - Vg(1))**2
       Vpg(2) = (Vp(2) - Vg(2))**2
       Vpg(3) = (Vp(3) - Vg(3))**2
 
-      Vs = sqrt(dot_product(Vp-Vg,Vp-Vg))
+      Vs = dsqrt(dot_product(Vp-Vg,Vp-Vg))
 
       Fd(:) = (3.0d0*RO_g0)/(4.0d0*RO_s0(1)*D_p0(1)) * Vpg(:)*Cd(Re(Vs))
 
-      return
       end function Fd
 
-      END MODULE usr
+      end module usr
