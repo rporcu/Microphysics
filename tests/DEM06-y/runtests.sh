60559b1e214e3d41f54ffd5a53c0a71321c557ad
Jordan Musser <jordan.musser@netl.doe.gov>
2017-06-23 15:43:55 -0400

    First pass at external udf for particle data

--- tests/DEM06-y/runtests.sh
+++ tests/DEM06-y/runtests.sh
@@ -11,6 +11,7 @@ fi
 
 if [ -n "$2" ]; then
     FCOMPARE=$2/plt_compare_diff_grids
+    FJOIN_PAR=$2/fjoin_par
 fi
 
 INPUTS=inputs_single
@@ -31,7 +32,10 @@ FCOMPARE=${FCOMPARE:-}
 rm -rf POST_* ${RUN_NAME}* &> /dev/null
 time -p ${MPIRUN} ${MFIX} "${INPUTS}" DES_ONEWAY_COUPLED=.F.
 
-post_dats=POST*.dat
+${FJOIN_PAR} -f DEM06_par --end 350 --var  2 --format 5 --dt 0.001 &> POST_POS.NEW
+${FJOIN_PAR} -f DEM06_par --end 350 --var 10 --format 4 --dt 0.001 &> POST_VEL.NEW
+
+post_dats=POST*.NEW
 for result in ${post_dats}; do
     diff -w -B "AUTOTEST/${result}" "${result}"
 done
