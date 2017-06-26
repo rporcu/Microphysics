8ec690dd99b4ee1720c2c1e6aeed660043db2017
Jordan Musser <jordan.musser@netl.doe.gov>
2017-06-26 15:02:22 -0400

    Add restart to DEM06-x/y/z cases

--- tests/DEM06-z/runtests.sh
+++ tests/DEM06-z/runtests.sh
@@ -30,7 +30,8 @@ MFIX_BENCHMARKS_HOME=${MFIX_BENCHMARKS_HOME:-}
 FCOMPARE=${FCOMPARE:-}
 
 rm -rf POST_* ${RUN_NAME}* &> /dev/null
-time -p ${MPIRUN} ${MFIX} "${INPUTS}" DES_ONEWAY_COUPLED=.F.
+time -p ${MPIRUN} ${MFIX} "${INPUTS}" TSTOP=0.150
+time -p ${MPIRUN} ${MFIX} "${INPUTS}" "amr.restart_file=DEM06_chk00150"
 
 ${FJOIN_PAR} -f DEM06_par --end 350 --var  3 --format 5 --dt 0.001 &> POST_POS.NEW
 ${FJOIN_PAR} -f DEM06_par --end 350 --var 11 --format 4 --dt 0.001 &> POST_VEL.NEW
