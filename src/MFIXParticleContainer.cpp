b82536eb0283fc279895cfbcf046e46c171fd0ce
Ann Almgren <asalmgren@lbl.gov>
2017-07-25 17:09:27 -0700

    Add the ability to replicate the domain when starting from a checkpoint file. NOTE -- we still need to hack the chk*****/particles/Header file in order for this to work correctly.

--- src/MFIXParticleContainer.cpp
+++ src/MFIXParticleContainer.cpp
@@ -107,6 +107,69 @@ void MFIXParticleContainer::InitParticlesAscii(const std::string& file) {
   Redistribute();
 }
 
+void MFIXParticleContainer::Replicate(IntVect& Nrep, Array<Real>& orig_domain_size)
+{
+    int lev = 0;
+    ParticleType p_rep;
+
+    for (MFIXParIter pti(*this, lev); pti.isValid(); ++pti)
+    {
+        auto& particles = pti.GetArrayOfStructs();
+
+        for (const auto& p: particles)
+        {
+           const IntVect& iv = Index(p, lev);
+
+           //
+           // Shift the position.
+           //
+           for (int k = 0; k < Nrep[2]; k++) {
+               for (int j = 0; j < Nrep[1]; j++) {
+                 for (int i = 0; i < Nrep[0]; i++) {
+
+                   if ( !(i == 0 && j == 0 && k == 0) ) 
+                   {
+
+                    p_rep.m_rdata.pos[0] = p.m_rdata.pos[0] + i * orig_domain_size[0];
+                    p_rep.m_rdata.pos[1] = p.m_rdata.pos[1] + j * orig_domain_size[1];
+                    p_rep.m_rdata.pos[2] = p.m_rdata.pos[2] + k * orig_domain_size[2];
+
+                    p_rep.rdata(realData::velx)   = p.rdata(realData::velx);
+                    p_rep.rdata(realData::vely)   = p.rdata(realData::vely);
+                    p_rep.rdata(realData::velz)   = p.rdata(realData::velz);
+
+                    // Set other particle properties
+                    p_rep.idata(intData::phase)     = p.idata(intData::phase);
+                    p_rep.idata(intData::state)     = p.idata(intData::state);
+                    p_rep.rdata(realData::volume)   = p.rdata(realData::volume);
+                    p_rep.rdata(realData::density)  = p.rdata(realData::density);
+                    p_rep.rdata(realData::mass)     = p.rdata(realData::mass);
+                    p_rep.rdata(realData::oneOverI) = p.rdata(realData::oneOverI);
+                    p_rep.rdata(realData::radius)   = p.rdata(realData::radius);
+                    p_rep.rdata(realData::omegax)   = p.rdata(realData::omegax);
+                    p_rep.rdata(realData::omegay)   = p.rdata(realData::omegay);
+                    p_rep.rdata(realData::omegaz)   = p.rdata(realData::omegaz);
+                    p_rep.rdata(realData::dragx)    = p.rdata(realData::dragx);
+                    p_rep.rdata(realData::dragy)    = p.rdata(realData::dragy);
+                    p_rep.rdata(realData::dragz)    = p.rdata(realData::dragz);
+
+                    // Set id and cpu for this particle
+                    p_rep.id()  = ParticleType::NextID();
+                    p_rep.cpu() = ParallelDescriptor::MyProc();
+
+                    // Add everything to the data structure
+                    particles.push_back(p_rep);
+
+                   } // not copying itself
+                 } // i
+              } // j
+           } // k
+        } // p
+    } // pti
+
+    Redistribute();
+}
+
 void MFIXParticleContainer:: printParticles()
 {
     const int lev = 0;
@@ -481,11 +544,7 @@ void MFIXParticleContainer::PICMultiDeposition(amrex::MultiFab& beta_mf, amrex::
     const Real*     dx_particle = Geom(lev).CellSize();
     const Real*     dx          = gm.CellSize();
     
-#ifdef _OPENMP
-#pragma omp parallel
-#endif 
-    for (MFIter mfi(*mf_pointer, true); mfi.isValid(); ++mfi) 
-        (*mf_pointer)[mfi].setVal(0);
+    mf_pointer->setVal(0.0);
 
     using ParConstIter = ParConstIter<realData::count,intData::count,0,0>;
 
@@ -551,10 +610,7 @@ void MFIXParticleContainer::output(int lev, int estatus, int finish, int nstep, 
     Real ylen = Geom(lev).ProbHi(1) - Geom(lev).ProbLo(1);
     Real zlen = Geom(lev).ProbHi(2) - Geom(lev).ProbLo(2);
 
-    //EP - should this be threaded since its output(?) 
-#ifdef _OPENMP
-#pragma omp parallel
-#endif
+    // Not threaded because its writing output
     for (MFIXParIter pti(*this, lev); pti.isValid(); ++pti)
     {
 
@@ -570,7 +626,7 @@ void MFIXParticleContainer::output(int lev, int estatus, int finish, int nstep, 
 
 void MFIXParticleContainer::writeAllAtLevel(int lev)
 {
-    //EP - not threaded because its print to terminal(?)
+    // Not threaded because its print to terminal
     for (MFIXParIter pti(*this, lev); pti.isValid(); ++pti)
     {
         auto& particles = pti.GetArrayOfStructs();
@@ -589,7 +645,7 @@ void MFIXParticleContainer::writeAllAtLevel(int lev)
 
 void MFIXParticleContainer::writeAllForComparison(int lev)
 {
-  size_t Np_tot = 0;
+  int Np_tot = 0;
 
 #ifdef _OPENMP
 #pragma omp parallel reduction(+:Np_tot)
@@ -597,11 +653,13 @@ void MFIXParticleContainer::writeAllForComparison(int lev)
   for (MFIXParIter pti(*this, lev); pti.isValid(); ++pti)
       Np_tot += pti.numParticles();
 
+  ParallelDescriptor::ReduceIntSum(Np_tot,ParallelDescriptor::IOProcessorNumber());
+
   cout << Np_tot << std::endl;
 
   Real dummy = 0.;
 
- //EP - not threaded because its print to terminal(?)
+  // Not threaded because its print to terminal
   for (MFIXParIter pti(*this, lev); pti.isValid(); ++pti)
   {
       auto& particles = pti.GetArrayOfStructs();
