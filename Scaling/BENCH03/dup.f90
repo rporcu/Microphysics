0ce8d86a23dcfb094013520b456bc33e96d58826
Jordan Musser <jordan.musser@netl.doe.gov>
2017-08-05 14:39:21 -0400

    Tweaks to bench03

--- Scaling/BENCH03/dup.f90
+++ Scaling/BENCH03/dup.f90
@@ -1,65 +1,55 @@
 program main
 
+implicit none
+
 integer :: scale
 
 integer :: pcount, m, lc, lc1, lc2
+integer :: np0, np
 
 double precision, allocatable :: particles(:,:)
+character(len=4) :: char4
 
-integer :: narg
+write(*,"(/2x,'Reading initial particles data')")
 
-character(len=256) :: fname
-
-narg = command_argument_count()
-
-if(narg /=1 ) stop 3322
-
-call get_command_argument(narg, value = fname)
-
-read(fname,*) scale
-
-open(file='../Size001/particle_input.dat',unit=1000)
-
+open(file='Size0001/particle_input.dat',unit=1000)
 read(1000,*) pcount
-
-write(*,*) 'particle count',pcount
-
 allocate(particles(pcount,8))
-
 do lc=1,pcount
-
    read(1000,*)m, particles(lc,1:8)
-
 enddo
-
 close(1000)
 
-open(file='particle_input.dat',unit=1000,status='unknown')
+write(*,"(/13x,'Size0001.  Particle Count: ',I8)") pcount
 
-write(1000,*) 2500*scale*scale
+do scale=1,5
+   np = 0
+   np0 = pcount*(4**scale)
+   write(char4,"(I4.4)") 4**scale
 
-do lc2=0,(scale-1)
+   write(*,"(2x,'Generating Size',A4,'.  Particle Count: ',I8)") char4, np0
 
-  do lc1=0,(scale-1)
+   open(file='Size'//char4//'/particle_input.dat',unit=1000,status='unknown')
+   write(1000,*) np0
 
-     do lc=1,pcount
+   do lc2=0,2**scale-1
+      do lc1=0,2**scale-1
+         do lc=1,pcount
+            write(1000,*) 1, &
+               particles(lc,1)+dble(lc1)*0.0008d0, &
+               particles(lc,2), &
+               particles(lc,3)+dble(lc2)*0.0008d0, &
+               particles(lc,4:8)
+            np = np + 1
+         enddo
+      enddo
+   enddo
+   close(1000)
 
-       write(1000,*) 1, &
-
-          particles(lc,1)+dble(lc1)*0.0008d0, &
-
-          particles(lc,2), &
-
-          particles(lc,3)+dble(lc2)*0.0008d0, &
-
-          particles(lc,4:8)
-
-     enddo
-
-  enddo
-
+   if(np /= np0) then
+      write(*,*) 'particle mismatch :: ',np0, np
+      stop 3322
+   endif
 enddo
-
-close(1000)
 
 end program main
