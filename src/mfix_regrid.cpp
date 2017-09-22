cb139552dbd15afbbda87680dd305e89a8cea68b
Ann Almgren <asalmgren@lbl.gov>
2017-09-22 06:29:52 -0700

    Modify the copy on regrid so that we copy ghost cells as well as valid

--- src/mfix_regrid.cpp
+++ src/mfix_regrid.cpp
@@ -10,7 +10,7 @@ mfix_level::Regrid (int lev, int nstep)
 {
     amrex::Print() << "In Regrid at step " << nstep << std::endl;
 
-    bool dual_grid = false;
+    bool dual_grid = true;
 
     if (load_balance_type == "KDTree")
     {
@@ -39,63 +39,75 @@ mfix_level::RegridArrays (int lev, BoxArray& new_grids, DistributionMapping& new
     // ********************************************************************************
 
     // Void fraction
-    std::unique_ptr<MultiFab> ep_g_new(new MultiFab(new_grids,new_dmap,1,ep_g[lev]->nGrow()));
-    ep_g_new->copy(*ep_g[lev]);
+    int ng = ep_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> ep_g_new(new MultiFab(new_grids,new_dmap,1,ng));
+    ep_g_new->copy(*ep_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ep_g[lev] = std::move(ep_g_new);
 
     // Old void fraction
+    ng = ep_go[lev]->nGrow();
     std::unique_ptr<MultiFab> ep_go_new(new MultiFab(new_grids,new_dmap,1,ep_go[lev]->nGrow()));
-    ep_go_new->copy(*ep_go[lev]);
+    ep_go_new->copy(*ep_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ep_go[lev] = std::move(ep_go_new);
 
     // Gas pressure fraction
+    ng = p_g[lev]->nGrow();
     std::unique_ptr<MultiFab> p_g_new(new MultiFab(new_grids,new_dmap,1,p_g[lev]->nGrow()));
-    p_g_new->copy(*p_g[lev]);
+    p_g_new->copy(*p_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     p_g[lev] = std::move(p_g_new);
 
     // Old gas pressure fraction
+    ng = p_go[lev]->nGrow();
     std::unique_ptr<MultiFab> p_go_new(new MultiFab(new_grids,new_dmap,1,p_go[lev]->nGrow()));
-    p_go_new->copy(*p_go[lev]);
+    p_go_new->copy(*p_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     p_go[lev] = std::move(p_go_new);
 
     // Gas density
+    ng = ro_g[lev]->nGrow();
     std::unique_ptr<MultiFab> ro_g_new(new MultiFab(new_grids,new_dmap,1,ro_g[lev]->nGrow()));
-    ro_g_new->copy(*ro_g[lev]);
+    ro_g_new->copy(*ro_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ro_g[lev] = std::move(ro_g_new);
 
     // Old gas density
+    ng = ro_go[lev]->nGrow();
     std::unique_ptr<MultiFab> ro_go_new(new MultiFab(new_grids,new_dmap,1,ro_go[lev]->nGrow()));
-    ro_go_new->copy(*ro_go[lev]);
+    ro_go_new->copy(*ro_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ro_go[lev] = std::move(ro_go_new);
 
     // Gas bulk density
+    ng = rop_g[lev]->nGrow();
     std::unique_ptr<MultiFab> rop_g_new(new MultiFab(new_grids,new_dmap,1,rop_g[lev]->nGrow()));
-    rop_g_new->copy(*rop_g[lev]);
+    rop_g_new->copy(*rop_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     rop_g[lev] = std::move(rop_g_new);
 
     // Old gas bulk density
+    ng = rop_go[lev]->nGrow();
     std::unique_ptr<MultiFab> rop_go_new(new MultiFab(new_grids,new_dmap,1,rop_go[lev]->nGrow()));
-    rop_go_new->copy(*rop_go[lev]);
+    rop_go_new->copy(*rop_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     rop_go[lev] = std::move(rop_go_new);
 
     // Pressure correction equation
+    ng = pp_g[lev]->nGrow();
     std::unique_ptr<MultiFab> pp_g_new(new MultiFab(new_grids,new_dmap,1,pp_g[lev]->nGrow()));
-    pp_g_new->copy(*pp_g[lev]);
+    pp_g_new->copy(*pp_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     pp_g[lev] = std::move(pp_g_new);
 
     // Molecular viscosity 
+    ng = mu_g[lev]->nGrow();
     std::unique_ptr<MultiFab> mu_g_new(new MultiFab(new_grids,new_dmap,1,mu_g[lev]->nGrow()));
-    mu_g_new->copy(*mu_g[lev]);
+    mu_g_new->copy(*mu_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     mu_g[lev] = std::move(mu_g_new);
 
     // Lambda
+    ng = lambda_g[lev]->nGrow();
     std::unique_ptr<MultiFab> lambda_g_new(new MultiFab(new_grids,new_dmap,1,lambda_g[lev]->nGrow()));
-    lambda_g_new->copy(*lambda_g[lev]);
+    lambda_g_new->copy(*lambda_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     lambda_g[lev] = std::move(lambda_g_new);
 
     // Trace(D)
+    ng = trD_g[lev]->nGrow();
     std::unique_ptr<MultiFab> trD_g_new(new MultiFab(new_grids,new_dmap,1,trD_g[lev]->nGrow()));
-    trD_g_new->copy(*trD_g[lev]);
+    trD_g_new->copy(*trD_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     trD_g[lev] = std::move(trD_g_new);
 
     // ********************************************************************************
@@ -107,40 +119,49 @@ mfix_level::RegridArrays (int lev, BoxArray& new_grids, DistributionMapping& new
     x_edge_ba.surroundingNodes(0);
 
     // X-axis gas velocity
-    std::unique_ptr<MultiFab> u_g_new(new MultiFab(x_edge_ba,new_dmap,1,u_g[lev]->nGrow()));
-    u_g_new->copy(*u_g[lev]);
+    ng = u_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> u_g_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    u_g_new->copy(*u_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     u_g[lev] = std::move(u_g_new);
 
-    std::unique_ptr<MultiFab> u_go_new(new MultiFab(x_edge_ba,new_dmap,1,u_go[lev]->nGrow()));
-    u_go_new->copy(*u_go[lev]);
+    ng = u_go[lev]->nGrow();
+    std::unique_ptr<MultiFab> u_go_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    u_go_new->copy(*u_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     u_go[lev] = std::move(u_go_new);
 
-    std::unique_ptr<MultiFab> u_gt_new(new MultiFab(x_edge_ba,new_dmap,1,u_gt[lev]->nGrow()));
-    u_gt_new->copy(*u_gt[lev]);
+    ng = u_gt[lev]->nGrow();
+    std::unique_ptr<MultiFab> u_gt_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    u_gt_new->copy(*u_gt[lev],0,0,1,ng,ng,geom[lev].periodicity());
     u_gt[lev] = std::move(u_gt_new);
 
-    std::unique_ptr<MultiFab> d_e_new(new MultiFab(x_edge_ba,new_dmap,1,d_e[lev]->nGrow()));
-    d_e_new->copy(*d_e[lev]);
+    ng = d_e[lev]->nGrow();
+    std::unique_ptr<MultiFab> d_e_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    d_e_new->copy(*d_e[lev],0,0,1,ng,ng,geom[lev].periodicity());
     d_e[lev] = std::move(d_e_new);
 
-    std::unique_ptr<MultiFab> tau_u_g_new(new MultiFab(x_edge_ba,new_dmap,1,tau_u_g[lev]->nGrow()));
-    tau_u_g_new->copy(*tau_u_g[lev]);
+    ng = tau_u_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> tau_u_g_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    tau_u_g_new->copy(*tau_u_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     tau_u_g[lev] = std::move(tau_u_g_new);
 
-    std::unique_ptr<MultiFab> fluxX_new(new MultiFab(x_edge_ba,new_dmap,1,fluxX[lev]->nGrow()));
-    fluxX_new->copy(*fluxX[lev]);
+    ng = fluxX[lev]->nGrow();
+    std::unique_ptr<MultiFab> fluxX_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    fluxX_new->copy(*fluxX[lev],0,0,1,ng,ng,geom[lev].periodicity());
     fluxX[lev] = std::move(fluxX_new);
 
-    std::unique_ptr<MultiFab> ropX_new(new MultiFab(x_edge_ba,new_dmap,1,ropX[lev]->nGrow()));
-    ropX_new->copy(*ropX[lev]);
+    ng = ropX[lev]->nGrow();
+    std::unique_ptr<MultiFab> ropX_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    ropX_new->copy(*ropX[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ropX[lev] = std::move(ropX_new);
 
-    std::unique_ptr<MultiFab> f_gds_u_new(new MultiFab(x_edge_ba,new_dmap,1,f_gds_u[lev]->nGrow()));
-    f_gds_u_new->copy(*f_gds_u[lev]);
+    ng = f_gds_u[lev]->nGrow();
+    std::unique_ptr<MultiFab> f_gds_u_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    f_gds_u_new->copy(*f_gds_u[lev],0,0,1,ng,ng,geom[lev].periodicity());
     f_gds_u[lev] = std::move(f_gds_u_new);
 
-    std::unique_ptr<MultiFab> drag_u_new(new MultiFab(x_edge_ba,new_dmap,1,drag_u[lev]->nGrow()));
-    drag_u_new->copy(*drag_u[lev]);
+    ng = drag_u[lev]->nGrow();
+    std::unique_ptr<MultiFab> drag_u_new(new MultiFab(x_edge_ba,new_dmap,1,ng));
+    drag_u_new->copy(*drag_u[lev],0,0,1,ng,ng,geom[lev].periodicity());
     drag_u[lev] = std::move(drag_u_new);
 
     // ********************************************************************************
@@ -152,40 +173,49 @@ mfix_level::RegridArrays (int lev, BoxArray& new_grids, DistributionMapping& new
     y_edge_ba.surroundingNodes(1);
 
     // Y-axis gas velocity
-    std::unique_ptr<MultiFab> v_g_new(new MultiFab(y_edge_ba,new_dmap,1,v_g[lev]->nGrow()));
-    v_g_new->copy(*v_g[lev]);
+    ng = v_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> v_g_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    v_g_new->copy(*v_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     v_g[lev] = std::move(v_g_new);
 
-    std::unique_ptr<MultiFab> v_go_new(new MultiFab(y_edge_ba,new_dmap,1,v_go[lev]->nGrow()));
-    v_go_new->copy(*v_go[lev]);
+    ng = v_go[lev]->nGrow();
+    std::unique_ptr<MultiFab> v_go_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    v_go_new->copy(*v_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     v_go[lev] = std::move(v_go_new);
 
-    std::unique_ptr<MultiFab> v_gt_new(new MultiFab(y_edge_ba,new_dmap,1,v_gt[lev]->nGrow()));
-    v_gt_new->copy(*v_gt[lev]);
+    ng = v_gt[lev]->nGrow();
+    std::unique_ptr<MultiFab> v_gt_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    v_gt_new->copy(*v_gt[lev],0,0,1,ng,ng,geom[lev].periodicity());
     v_gt[lev] = std::move(v_gt_new);
 
-    std::unique_ptr<MultiFab> d_n_new(new MultiFab(y_edge_ba,new_dmap,1,d_n[lev]->nGrow()));
-    d_n_new->copy(*d_n[lev]);
+    ng = d_n[lev]->nGrow();
+    std::unique_ptr<MultiFab> d_n_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    d_n_new->copy(*d_n[lev],0,0,1,ng,ng,geom[lev].periodicity());
     d_n[lev] = std::move(d_n_new);
 
-    std::unique_ptr<MultiFab> tau_v_g_new(new MultiFab(y_edge_ba,new_dmap,1,tau_v_g[lev]->nGrow()));
-    tau_v_g_new->copy(*tau_v_g[lev]);
+    ng = tau_v_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> tau_v_g_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    tau_v_g_new->copy(*tau_v_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     tau_v_g[lev] = std::move(tau_v_g_new);
 
-    std::unique_ptr<MultiFab> fluxY_new(new MultiFab(y_edge_ba,new_dmap,1,fluxY[lev]->nGrow()));
-    fluxY_new->copy(*fluxY[lev]);
+    ng = fluxY[lev]->nGrow();
+    std::unique_ptr<MultiFab> fluxY_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    fluxY_new->copy(*fluxY[lev],0,0,1,ng,ng,geom[lev].periodicity());
     fluxY[lev] = std::move(fluxY_new);
 
-    std::unique_ptr<MultiFab> ropY_new(new MultiFab(y_edge_ba,new_dmap,1,ropY[lev]->nGrow()));
-    ropY_new->copy(*ropY[lev]);
+    ng = ropY[lev]->nGrow();
+    std::unique_ptr<MultiFab> ropY_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    ropY_new->copy(*ropY[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ropY[lev] = std::move(ropY_new);
 
-    std::unique_ptr<MultiFab> f_gds_v_new(new MultiFab(y_edge_ba,new_dmap,1,f_gds_v[lev]->nGrow()));
-    f_gds_v_new->copy(*f_gds_v[lev]);
+    ng = f_gds_v[lev]->nGrow();
+    std::unique_ptr<MultiFab> f_gds_v_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    f_gds_v_new->copy(*f_gds_v[lev],0,0,1,ng,ng,geom[lev].periodicity());
     f_gds_v[lev] = std::move(f_gds_v_new);
 
-    std::unique_ptr<MultiFab> drag_v_new(new MultiFab(y_edge_ba,new_dmap,1,drag_v[lev]->nGrow()));
-    drag_v_new->copy(*drag_v[lev]);
+    ng = drag_v[lev]->nGrow();
+    std::unique_ptr<MultiFab> drag_v_new(new MultiFab(y_edge_ba,new_dmap,1,ng));
+    drag_v_new->copy(*drag_v[lev],0,0,1,ng,ng,geom[lev].periodicity());
     drag_v[lev] = std::move(drag_v_new);
 
     // ********************************************************************************
@@ -197,40 +227,49 @@ mfix_level::RegridArrays (int lev, BoxArray& new_grids, DistributionMapping& new
     z_edge_ba.surroundingNodes(2);
 
     // Z-axis gas velocity
-    std::unique_ptr<MultiFab> w_g_new(new MultiFab(z_edge_ba,new_dmap,1,w_g[lev]->nGrow()));
-    w_g_new->copy(*w_g[lev]);
+    ng = w_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> w_g_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    w_g_new->copy(*w_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     w_g[lev] = std::move(w_g_new);
 
-    std::unique_ptr<MultiFab> w_go_new(new MultiFab(z_edge_ba,new_dmap,1,w_go[lev]->nGrow()));
-    w_go_new->copy(*w_go[lev]);
+    ng = w_go[lev]->nGrow();
+    std::unique_ptr<MultiFab> w_go_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    w_go_new->copy(*w_go[lev],0,0,1,ng,ng,geom[lev].periodicity());
     w_go[lev] = std::move(w_go_new);
 
-    std::unique_ptr<MultiFab> w_gt_new(new MultiFab(z_edge_ba,new_dmap,1,w_gt[lev]->nGrow()));
-    w_gt_new->copy(*w_gt[lev]);
+    ng = w_gt[lev]->nGrow();
+    std::unique_ptr<MultiFab> w_gt_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    w_gt_new->copy(*w_gt[lev],0,0,1,ng,ng,geom[lev].periodicity());
     w_gt[lev] = std::move(w_gt_new);
 
-    std::unique_ptr<MultiFab> d_t_new(new MultiFab(z_edge_ba,new_dmap,1,d_t[lev]->nGrow()));
-    d_t_new->copy(*d_t[lev]);
+    ng = d_t[lev]->nGrow();
+    std::unique_ptr<MultiFab> d_t_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    d_t_new->copy(*d_t[lev],0,0,1,ng,ng,geom[lev].periodicity());
     d_t[lev] = std::move(d_t_new);
 
-    std::unique_ptr<MultiFab> tau_w_g_new(new MultiFab(z_edge_ba,new_dmap,1,tau_w_g[lev]->nGrow()));
-    tau_w_g_new->copy(*tau_w_g[lev]);
+    ng = tau_w_g[lev]->nGrow();
+    std::unique_ptr<MultiFab> tau_w_g_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    tau_w_g_new->copy(*tau_w_g[lev],0,0,1,ng,ng,geom[lev].periodicity());
     tau_w_g[lev] = std::move(tau_w_g_new);
 
-    std::unique_ptr<MultiFab> fluxZ_new(new MultiFab(z_edge_ba,new_dmap,1,fluxZ[lev]->nGrow()));
-    fluxZ_new->copy(*fluxZ[lev]);
+    ng = fluxZ[lev]->nGrow();
+    std::unique_ptr<MultiFab> fluxZ_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    fluxZ_new->copy(*fluxZ[lev],0,0,1,ng,ng,geom[lev].periodicity());
     fluxZ[lev] = std::move(fluxZ_new);
 
-    std::unique_ptr<MultiFab> ropZ_new(new MultiFab(z_edge_ba,new_dmap,1,ropZ[lev]->nGrow()));
-    ropZ_new->copy(*ropZ[lev]);
+    ng = ropZ[lev]->nGrow();
+    std::unique_ptr<MultiFab> ropZ_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    ropZ_new->copy(*ropZ[lev],0,0,1,ng,ng,geom[lev].periodicity());
     ropZ[lev] = std::move(ropZ_new);
 
-    std::unique_ptr<MultiFab> f_gds_w_new(new MultiFab(z_edge_ba,new_dmap,1,f_gds_w[lev]->nGrow()));
-    f_gds_w_new->copy(*f_gds_w[lev]);
+    ng = f_gds_w[lev]->nGrow();
+    std::unique_ptr<MultiFab> f_gds_w_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    f_gds_w_new->copy(*f_gds_w[lev],0,0,1,ng,ng,geom[lev].periodicity());
     f_gds_w[lev] = std::move(f_gds_w_new);
 
-    std::unique_ptr<MultiFab> drag_w_new(new MultiFab(z_edge_ba,new_dmap,1,drag_w[lev]->nGrow()));
-    drag_w_new->copy(*drag_w[lev]);
+    ng = drag_w[lev]->nGrow();
+    std::unique_ptr<MultiFab> drag_w_new(new MultiFab(z_edge_ba,new_dmap,1,ng));
+    drag_w_new->copy(*drag_w[lev],0,0,1,ng,ng,geom[lev].periodicity());
     drag_w[lev] = std::move(drag_w_new);
 
     // ********************************************************************************
