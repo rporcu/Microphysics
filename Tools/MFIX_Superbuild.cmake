7adac13590a60ea9f787e8331c77e19230e90e8b
Ann Almgren <asalmgren@lbl.gov>
2017-10-20 17:57:35 -0700

    Update the amrex hash so that we can build with the EB stuff.

--- Tools/MFIX_Superbuild.cmake
+++ Tools/MFIX_Superbuild.cmake
@@ -33,7 +33,7 @@ set (CMAKE_CXX_EXTENSIONS OFF)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER  3506f5aea50d27237dda43df3ba4611fd4eda638 )
-set (AMREX_GIT_COMMIT_DEVELOP 396a3fae0a2d3a0a2909de188fc76ba484c99b01 )
+set (AMREX_GIT_COMMIT_DEVELOP d2ce70d0d0717a557b0aeb487da367e0 )
 set (AMREX_GIT_TAG)  # The commit id or branch to download 
 
 # AMReX Superbuild variables
