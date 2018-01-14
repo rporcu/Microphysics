f94ec8a0ef4912b6d96210ae68e3f3aa7bd6122d
Ann Almgren <asalmgren@lbl.gov>
2018-01-13 18:28:38 -0800

    Update hash for develop version so we get the periodic fill of the EB "flag"

--- Tools/MFIX_Superbuild.cmake
+++ Tools/MFIX_Superbuild.cmake
@@ -33,7 +33,7 @@ set (CMAKE_CXX_EXTENSIONS OFF)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER  3506f5aea50d27237dda43df3ba4611fd4eda638 )
-set (AMREX_GIT_COMMIT_DEVELOP 23a04c4a6aa70f2f818341d6adedc3 )
+set (AMREX_GIT_COMMIT_DEVELOP 1dd104b45dff38c40515df )
 set (AMREX_GIT_TAG)  # The commit id or branch to download 
 
 #
