e05c211582610c21381410b846a5e3c36c47b234
Michele Rosso <mrosso@lbl.gov>
2018-01-23 10:50:16 -0800

    Add projection-enabled FLD03 tests

--- tests/FLD03-z_P/runtests.sh
+++ tests/FLD03-z_P/runtests.sh
@@ -40,8 +40,8 @@ FCOMPARE=${FCOMPARE:-}
 rm -rf POST_* ${RUN_NAME}* &> /dev/null
 time -p ${MPIRUN} "${MFIX}" "${INPUTS}"
 
-${FEXTRACT} -p FLD0300001/ -d 2 -v u_g -s POST_VG.dat
-${FEXTRACT} -p FLD0300001/ -d 1 -v p_g -s POST_PG.dat
+${FEXTRACT} -p FLD0300001/ -d 1 -v w_g -s POST_VG.dat
+${FEXTRACT} -p FLD0300001/ -d 3 -v p_g -s POST_PG.dat
 
 post_dats=POST*.dat
 for result in ${post_dats}; do
