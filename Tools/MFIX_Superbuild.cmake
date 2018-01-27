d97095b9c9e2077f3ca743cc6b5c9262bb96bb81
Andrew Myers <atmyers2@gmail.com>
2018-01-26 20:58:15 -0800

    bump AMReX Version

--- Tools/MFIX_Superbuild.cmake
+++ Tools/MFIX_Superbuild.cmake
@@ -32,7 +32,7 @@ set (CMAKE_CXX_EXTENSIONS OFF)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER  3506f5aea50d27237dda43df3ba4611fd4eda638 )
-set (AMREX_GIT_COMMIT_DEVELOP   a7d629b47d8f41c29a18c980c5c )
+set (AMREX_GIT_COMMIT_DEVELOP   eeb1fae5375901b78607bd7fe7743434aeee4a58 )
 set (AMREX_GIT_TAG)  # The commit id or branch to download 
 
 #
