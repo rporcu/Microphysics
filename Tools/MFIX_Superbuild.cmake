b119a723be8c2119860c2d6243bba86aec945fc1
Ann Almgren <asalmgren@lbl.gov>
2018-02-19 12:44:03 -0800

    Update the amrex development version so we can use the STL tools

--- Tools/MFIX_Superbuild.cmake
+++ Tools/MFIX_Superbuild.cmake
@@ -32,7 +32,7 @@ set (CMAKE_CXX_EXTENSIONS OFF)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER  3506f5aea50d27237dda43df3ba4611fd4eda638 )
-set (AMREX_GIT_COMMIT_DEVELOP  711de9897227a3b366aa2c62deaab2c88f59f0cd )
+set (AMREX_GIT_COMMIT_DEVELOP  c6ae1650b60c3f7ef5db2a5880130f58e0bb8483 )
 set (AMREX_GIT_TAG)  # The commit id or branch to download 
 
 #
