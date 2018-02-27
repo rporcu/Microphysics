5ad1c7a3fcc0936fdc86a9d0987427658e90f730
Johannes Blaschke <jpblaschke@lbl.gov>
2018-02-27 09:51:34 -0800

    WIP:clean up

--- src/eb/mfix_eb.cpp
+++ src/eb/mfix_eb.cpp
@@ -154,29 +154,9 @@ mfix_level::make_eb_geometry(int lev)
     //            -> use min to intersect new eb boundaries
     level_set     = std::unique_ptr<LSFactory>(new LSFactory(lev, 1, 1, 2, 2, pc.get()));
 
-    //Box dom_eb = geom[lev].Domain();
-    //dom_eb.refine(level_set->get_eb_ref());
-    //Geometry geom_eb(dom_eb);
-    //amrex::Print() << "EB geom_eb: " << geom_eb << std::endl;
-
-    // refine geom_ls' Domain
-    //Box dom_ls = geom[lev].Domain();
-    //dom_ls.refine(level_set->get_ls_ref());
-    //Geometry geom_ls(dom_ls);
-    //amrex::Print() << "Refined geom_ls: " << geom_ls << std::endl;
-
-    //Geometry geom_eb_g = geom_eb;
-    //Box dom_eb_g = geom_eb.Domain();
-    //dom_eb_g.grow(4);
-    //geom_eb_g.Domain(dom_eb_g);
-    //amrex::Print() << "Grown geom_ls_g: " << geom_eb_g << std::endl;
-    //BoxArray ba_g = * level_set->get_eb_ba();
-    //ba_g.grow(4);
-
     GeometryShop gshop_poly2(* impfunc_poly2, true);
     GeometryShop gshop_walls(* impfunc_walls, true);
 
-    //AMReX_EBIS::instance()->define(dom_ls, RealVect::Zero, geom_eb.CellSize()[0], gshop_walls, 16, 0);
     Geometry geom_ls = LSUtility::make_ls_geometry(* level_set);
     AMReX_EBIS::instance()->define(geom_ls.Domain(), RealVect::Zero, geom_ls.CellSize()[0], gshop_walls, 16, 0);
 
@@ -184,12 +164,10 @@ mfix_level::make_eb_geometry(int lev)
     level_set->update_ebis(* AMReX_EBIS::instance());
     EBTower::Destroy();
 
-    //AMReX_EBIS::instance()->define(dom_eb_g, RealVect::Zero, geom_eb_g.CellSize()[0], gshop_poly2, 16, 0);
     Geometry geom_eb = LSUtility::make_eb_geometry(* level_set);
     AMReX_EBIS::instance()->define(geom_eb.Domain(), RealVect::Zero, geom_eb.CellSize()[0], gshop_poly2, 16, 0);
 
     EBTower::Build();
-    //EBFArrayBoxFactory eb_factory_poly2(geom_eb_g, ba_g, dmap[lev], {2, 2, 2}, EBSupport::full);
     EBFArrayBoxFactory eb_factory_poly2(geom_eb, * level_set->get_eb_ba(), dmap[lev], {2, 2, 2}, EBSupport::full);
     level_set->update_ebf(eb_factory_poly2, * AMReX_EBIS::instance());
     EBTower::Destroy();
