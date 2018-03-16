089ae60cc092106e67fc4ea0cb84ec0f035ec540
Ann Almgren <asalmgren@lbl.gov>
2018-03-16 10:42:45 -0700

    Fix the mass inflow to look like those in set_bc0 rather than copying from interior values

--- src/set_bc1.f90
+++ src/set_bc1.f90
@@ -16,9 +16,12 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
       use iso_c_binding , only: c_int
 
       use bc, only: pinf_, pout_, minf_, nsw_, fsw_, psw_
-      use bc, only: bc_v_g
-      use bc, only: bc_u_g, bc_v_g, bc_w_g
+      use bc, only: bc_u_g, bc_v_g, bc_w_g, bc_ep_g, bc_t_g
       use bc, only: bc_uw_g, bc_vw_g, bc_ww_g
+
+      use eos      , only: eosg, sutherland
+      use fld_const, only: ro_g0, mw_avg, mu_g0
+      use param    , only: is_undefined
 
       implicit none
 
@@ -63,8 +66,8 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
 !--------------------------------------------------------------------//
 ! local index for boundary condition
       integer :: bcv, i,j,k
-
-      integer    nlft, nrgt, nbot, ntop, nup, ndwn
+      integer :: nlft, nrgt, nbot, ntop, nup, ndwn
+      real(c_real) :: bc_ro_g, bc_mu_g, bc_lambda_g
 !--------------------------------------------------------------------//
 
       nlft = max(0,domlo(1)-slo(1))
@@ -74,6 +77,8 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
       nrgt = max(0,shi(1)-domhi(1))
       ntop = max(0,shi(2)-domhi(2))
       nup  = max(0,shi(3)-domhi(3))
+
+      bc_ro_g = ro_g0
 
       if (nlft .gt. 0) then
          do k=slo(3),shi(3)
@@ -101,11 +106,19 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
                        v_g(vlo(1):domlo(1)-1,j,k) = 0.0d0
                        w_g(wlo(1):domlo(1)-1,j,k) = 0.0d0
 
-                      ep_g(slo(1):domlo(1)-1,j,k) =     ep_g(domlo(1),j,k)
-                      ro_g(slo(1):domlo(1)-1,j,k) =     ro_g(domlo(1),j,k)
-                     rop_g(slo(1):domlo(1)-1,j,k) =    rop_g(domlo(1),j,k)
-                      mu_g(slo(1):domlo(1)-1,j,k) =     mu_g(domlo(1),j,k)
-                  lambda_g(slo(1):domlo(1)-1,j,k) = lambda_g(domlo(1),j,k)
+                       if (is_undefined(mu_g0)) then
+                          bc_mu_g     = sutherland(bc_t_g(bcv))
+                          bc_lambda_g = -(2.0d0/3.0d0) * bc_mu_g
+                       else
+                          bc_mu_g     = mu_g0
+                          bc_lambda_g = -(2.0d0/3.0d0) * mu_g0
+                       endif
+
+                      ep_g(slo(1):domlo(1)-1,j,k) = bc_ep_g(bcv)   
+                      ro_g(slo(1):domlo(1)-1,j,k) = bc_ro_g
+                     rop_g(slo(1):domlo(1)-1,j,k) = bc_ro_g * bc_ep_g(bcv)
+                      mu_g(slo(1):domlo(1)-1,j,k) = bc_mu_g
+                  lambda_g(slo(1):domlo(1)-1,j,k) = bc_lambda_g
 
                else if (bc_ilo_type(j,k,1) == NSW_) then
 
@@ -150,11 +163,19 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
                        v_g(domhi(1)+1:vhi(1),j,k) = 0.0d0
                        w_g(domhi(1)+1:whi(1),j,k) = 0.0d0
 
-                      ep_g(domhi(1)+1:shi(1),j,k) =     ep_g(domhi(1)  ,j,k)
-                      ro_g(domhi(1)+1:shi(1),j,k) =     ro_g(domhi(1)  ,j,k)
-                     rop_g(domhi(1)+1:shi(1),j,k) =    rop_g(domhi(1)  ,j,k)
-                      mu_g(domhi(1)+1:shi(1),j,k) =     mu_g(domhi(1)  ,j,k)
-                  lambda_g(domhi(1)+1:shi(1),j,k) = lambda_g(domhi(1)  ,j,k)
+                       if (is_undefined(mu_g0)) then
+                          bc_mu_g     = sutherland(bc_t_g(bcv))
+                          bc_lambda_g = -(2.0d0/3.0d0) * bc_mu_g
+                       else
+                          bc_mu_g     = mu_g0
+                          bc_lambda_g = -(2.0d0/3.0d0) * mu_g0
+                       endif
+
+                      ep_g(domhi(1)+1:shi(1),j,k) = bc_ep_g(bcv)   
+                      ro_g(domhi(1)+1:shi(1),j,k) = bc_ro_g
+                     rop_g(domhi(1)+1:shi(1),j,k) = bc_ro_g * bc_ep_g(bcv)   
+                      mu_g(domhi(1)+1:shi(1),j,k) = bc_mu_g
+                  lambda_g(domhi(1)+1:shi(1),j,k) = bc_lambda_g
 
                else if (bc_ihi_type(j,k,1) == NSW_) then
 
@@ -200,11 +221,19 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
                        v_g(i,vlo(2):domlo(2)  ,k) = bc_v_g(bcv)
                        w_g(i,wlo(2):domlo(2)-1,k) = 0.0d0
 
-                      ep_g(i,slo(1):domlo(2)-1,k) =     ep_g(i,domlo(2),k)
-                      ro_g(i,slo(1):domlo(2)-1,k) =     ro_g(i,domlo(2),k)
-                     rop_g(i,slo(1):domlo(2)-1,k) =    rop_g(i,domlo(2),k)
-                      mu_g(i,slo(1):domlo(2)-1,k) =     mu_g(i,domlo(2),k)
-                  lambda_g(i,slo(1):domlo(2)-1,k) = lambda_g(i,domlo(2),k)
+                       if (is_undefined(mu_g0)) then
+                          bc_mu_g     = sutherland(bc_t_g(bcv))
+                          bc_lambda_g = -(2.0d0/3.0d0) * bc_mu_g
+                       else
+                          bc_mu_g     = mu_g0
+                          bc_lambda_g = -(2.0d0/3.0d0) * mu_g0
+                       endif
+
+                      ep_g(i,slo(1):domlo(2)-1,k) = bc_ep_g(bcv)   
+                      ro_g(i,slo(1):domlo(2)-1,k) = bc_ro_g
+                     rop_g(i,slo(1):domlo(2)-1,k) = bc_ro_g * bc_ep_g(bcv)   
+                      mu_g(i,slo(1):domlo(2)-1,k) = bc_mu_g
+                  lambda_g(i,slo(1):domlo(2)-1,k) = bc_lambda_g
 
                else if (bc_jlo_type(i,k,1) == NSW_)then
 
@@ -250,11 +279,19 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
                        v_g(i,domhi(2)+1:vhi(2),k) = bc_v_g(bcv)
                        w_g(i,domhi(2)+1:whi(2),k) = 0.0d0
 
-                      ep_g(i,domhi(2)+1:shi(2),k) =     ep_g(i,domhi(2)  ,k)
-                      ro_g(i,domhi(2)+1:shi(2),k) =     ro_g(i,domhi(2)  ,k)
-                     rop_g(i,domhi(2)+1:shi(2),k) =    rop_g(i,domhi(2)  ,k)
-                      mu_g(i,domhi(2)+1:shi(2),k) =     mu_g(i,domhi(2)  ,k)
-                  lambda_g(i,domhi(2)+1:shi(2),k) = lambda_g(i,domhi(2)  ,k)
+                       if (is_undefined(mu_g0)) then
+                          bc_mu_g     = sutherland(bc_t_g(bcv))
+                          bc_lambda_g = -(2.0d0/3.0d0) * bc_mu_g
+                       else
+                          bc_mu_g     = mu_g0
+                          bc_lambda_g = -(2.0d0/3.0d0) * mu_g0
+                       endif
+
+                      ep_g(i,domhi(2)+1:shi(2),k) = bc_ep_g(bcv)   
+                      ro_g(i,domhi(2)+1:shi(2),k) = bc_ro_g
+                     rop_g(i,domhi(2)+1:shi(2),k) = bc_ro_g * bc_ep_g(bcv)   
+                      mu_g(i,domhi(2)+1:shi(2),k) = bc_mu_g
+                  lambda_g(i,domhi(2)+1:shi(2),k) = bc_lambda_g
 
                else if (bc_jhi_type(i,k,1) == NSW_) then
 
@@ -299,11 +336,19 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
                        v_g(i,j,vlo(3):domlo(3)-1) = 0.0d0
                        w_g(i,j,wlo(3):domlo(3)  ) = bc_w_g(bcv)
 
-                      ep_g(i,j,slo(3):domlo(3)-1) =     ep_g(i,j,domlo(3))
-                      ro_g(i,j,slo(3):domlo(3)-1) =     ro_g(i,j,domlo(3))
-                     rop_g(i,j,slo(3):domlo(3)-1) =    rop_g(i,j,domlo(3))
-                      mu_g(i,j,slo(3):domlo(3)-1) =     mu_g(i,j,domlo(3))
-                  lambda_g(i,j,slo(3):domlo(3)-1) = lambda_g(i,j,domlo(3))
+                       if (is_undefined(mu_g0)) then
+                          bc_mu_g     = sutherland(bc_t_g(bcv))
+                          bc_lambda_g = -(2.0d0/3.0d0) * bc_mu_g
+                       else
+                          bc_mu_g     = mu_g0
+                          bc_lambda_g = -(2.0d0/3.0d0) * mu_g0
+                       endif
+
+                      ep_g(i,j,slo(3):domlo(3)-1) = bc_ep_g(bcv)   
+                      ro_g(i,j,slo(3):domlo(3)-1) = bc_ro_g
+                     rop_g(i,j,slo(3):domlo(3)-1) = bc_ro_g * bc_ep_g(bcv)   
+                      mu_g(i,j,slo(3):domlo(3)-1) = bc_mu_g
+                  lambda_g(i,j,slo(3):domlo(3)-1) = bc_lambda_g
 
                else if (bc_klo_type(i,j,1) == NSW_) then
 
@@ -347,11 +392,19 @@ subroutine set_bc1(slo, shi, ulo, uhi, vlo, vhi, wlo, whi, &
                        v_g(i,j,domhi(3)+1:vhi(3)) = 0.0d0
                        w_g(i,j,domhi(3)+1:whi(3)) = bc_w_g(bcv)
 
-                      ep_g(i,j,domhi(3)+1:shi(3)) =     ep_g(i,j,domhi(3)  )
-                      ro_g(i,j,domhi(3)+1:shi(3)) =     ro_g(i,j,domhi(3)  )
-                     rop_g(i,j,domhi(3)+1:shi(3)) =    rop_g(i,j,domhi(3)  )
-                      mu_g(i,j,domhi(3)+1:shi(3)) =     mu_g(i,j,domhi(3)  )
-                  lambda_g(i,j,domhi(3)+1:shi(3)) = lambda_g(i,j,domhi(3)  )
+                       if (is_undefined(mu_g0)) then
+                          bc_mu_g     = sutherland(bc_t_g(bcv))
+                          bc_lambda_g = -(2.0d0/3.0d0) * bc_mu_g
+                       else
+                          bc_mu_g     = mu_g0
+                          bc_lambda_g = -(2.0d0/3.0d0) * mu_g0
+                       endif
+
+                      ep_g(i,j,domhi(3)+1:shi(3)) = bc_ep_g(bcv)   
+                      ro_g(i,j,domhi(3)+1:shi(3)) = bc_ro_g
+                     rop_g(i,j,domhi(3)+1:shi(3)) = bc_ro_g * bc_ep_g(bcv)   
+                      mu_g(i,j,domhi(3)+1:shi(3)) = bc_mu_g
+                  lambda_g(i,j,domhi(3)+1:shi(3)) = bc_lambda_g
 
                else if (bc_khi_type(i,j,1) == NSW_) then
 
