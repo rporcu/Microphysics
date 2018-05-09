bc5cea861037ad5990e0900de092fc77497c65d1
Ann Almgren <asalmgren@lbl.gov>
2018-05-08 17:36:19 -0700

    Update amrex hash to make sure we stay compliant with namespace changes

--- Tools/MFIX_Superbuild.cmake
+++ Tools/MFIX_Superbuild.cmake
@@ -32,7 +32,7 @@ set (CMAKE_CXX_EXTENSIONS OFF)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER  3506f5aea50d27237dda43df3ba4611fd4eda638 )
-set (AMREX_GIT_COMMIT_DEVELOP 2832ad001c06a2e2ef0106e43a81620084e9d )
+set (AMREX_GIT_COMMIT_DEVELOP  f30e2bbf5a59e650d88562ad0260d )
 set (AMREX_GIT_TAG)  # The commit id or branch to download
 
 #
