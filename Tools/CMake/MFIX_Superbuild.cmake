5fdc52e8ade3a9fc35834c4e9cae5d9f1923b708
Ann Almgren <asalmgren@lbl.gov>
2018-07-19 12:29:39 -0700

    Update git hash for amrex

--- Tools/CMake/MFIX_Superbuild.cmake
+++ Tools/CMake/MFIX_Superbuild.cmake
@@ -26,7 +26,8 @@ enable_language (Fortran)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER  3506f5aea50d27237dda43df3ba4611fd4eda638 )
-set (AMREX_GIT_COMMIT_DEVELOP fc0af66e48009713b1e4d2cacc815590c09c2097 )
+set (AMREX_GIT_COMMIT_DEVELOP f3ccbe5cfde43464f47b779fb0e1ded6375e9710 )
+
 set (AMREX_GIT_TAG)  # The commit id or branch to download
 
 #
