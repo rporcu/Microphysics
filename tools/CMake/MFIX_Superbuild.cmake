45a830535a9b794ea4ec1e3065b3107b5db9e431
Ann Almgren <asalmgren@lbl.gov>
2018-08-17 11:15:33 -0700

    Add DEFINES += -DAMREX_USE_GEOMETRY_SHOP so that we can continue to run with GeometryShop even when AMReX converts to that not being the default

--- tools/CMake/MFIX_Superbuild.cmake
+++ tools/CMake/MFIX_Superbuild.cmake
@@ -26,7 +26,7 @@ enable_language (Fortran)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER   777a7c4241cb82437e06aabd7dfe0a32d4 )
-set (AMREX_GIT_COMMIT_DEVELOP  0828c263b8169c59e4cd9b53a163f25639 )
+set (AMREX_GIT_COMMIT_DEVELOP  f7b22e11feedbeeba494f7bfdc22ee144a7fb )
 
 set (AMREX_GIT_TAG)  # The commit id or branch to download
 
@@ -43,6 +43,8 @@ set (AMREX_Fortran_FLAGS "" CACHE STRING "User-defined Fortran compiler flags fo
 set (AMREX_CXX_FLAGS "" CACHE STRING "User-defined C++ compiler flags for AMReX (Superbuild only)" )
 
 option ( AMREX_ENABLE_EB "Build EB code" ON)
+
+option ( AMREX_ENABLE_GEOMETRY_SHOP "Use the old GeometryShop instead of EB2" ON )
 
 option ( AMREX_ENABLE_PIC "Build position-independent code" OFF)
 
@@ -117,6 +119,7 @@ ExternalProject_Add ( amrex
    -DENABLE_AMRDATA=ON # Needed to compile postprocessing tools
    -DENABLE_LINEAR_SOLVERS=ON
    -DENABLE_EB=${AMREX_ENABLE_EB}
+   -DENABLE_GEOMETRY_SHOP=${AMREX_ENABLE_GEOMETRY_SHOP}
    -DENABLE_FORTRAN_INTERFACES=OFF
    -DENABLE_BASE_PROFILE=${AMREX_ENABLE_BASE_PROFILE}
    -DENABLE_TINY_PROFILE=${AMREX_ENABLE_TINY_PROFILE}
