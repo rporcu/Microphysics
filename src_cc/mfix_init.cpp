ae6048536ca026a929ec084718bf525015bcc43e
Johannes Blaschke <jpblaschke@lbl.gov>
2018-09-05 18:46:45 -0700

    more hacking

--- src_cc/mfix_init.cpp
+++ src_cc/mfix_init.cpp
@@ -473,9 +473,9 @@ mfix_level::PostInit(int lev, Real dt, Real time, int nstep, int restart_flag, R
   }
 
   // Define the MultiFab dummy which we pass into some MFIXParticleContainer routines.
-  if (solve_dem)
-      dummy->define(pc->ParticleBoxArray(lev), pc->ParticleDistributionMap(lev), 1, 0,
-                    MFInfo(), * particle_ebfactory[lev]);
+  //if (solve_dem)
+  //    dummy->define(pc->ParticleBoxArray(lev), pc->ParticleDistributionMap(lev), 1, 0,
+  //                  MFInfo(), * particle_ebfactory[lev]);
 
   // Initial fluid arrays: pressure, velocity, density, viscosity
   if (solve_fluid)
