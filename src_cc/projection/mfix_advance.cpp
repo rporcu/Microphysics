2ad1ee6a2004c3c80e0cafdc1e332e452d7ceb1b
Ann Almgren <asalmgren@lbl.gov>
2018-09-09 08:45:20 -0700

    We now add -dt grad(p) to momenta not velocities -- this should be correct now

--- src_cc/projection/mfix_advance.cpp
+++ src_cc/projection/mfix_advance.cpp
@@ -217,13 +217,17 @@ mfix_level::mfix_apply_predictor (int lev, MultiFab& conv_old, MultiFab& divtau_
     // Add the forcing terms
     mfix_apply_forcing_terms ( lev, dt, vel_g);
 
-    // HACK HACK -- NO DIVIDE BY RHO HERE YET
-    // Add (-dt/rho grad p to velocity)
+    // Convert velocities to momenta
+    for (int n = 0; n < 3; n++)
+       MultiFab::Multiply(*vel_g[lev],(*ro_g[lev]),0,n,1,vel_g[lev]->nGrow());
+
+    // Add (-dt grad p to momenta)
     MultiFab::Saxpy (*vel_g[lev], -dt,  *gp[lev], 0, 0, 3, 0);
     MultiFab::Saxpy (*vel_g[lev], -dt, *gp0[lev], 0, 0, 3, 0);
 
-    // mfix_add_grad_phi ( lev, -dt, (*p_g[lev]) );
-    // mfix_add_grad_phi ( lev, -dt, (*p0_g[lev]) );
+    // Convert momenta back to velocities
+    for (int n = 0; n < 3; n++)
+       MultiFab::Divide(*vel_g[lev],(*ro_g[lev]),0,n,1,vel_g[lev]->nGrow());
 
     // If doing implicit diffusion, solve here for u^*
     if (!explicit_diffusion)
@@ -296,14 +300,17 @@ mfix_level::mfix_apply_corrector (int lev, MultiFab& conv_old, MultiFab& divtau_
     // Add forcing terms
     mfix_apply_forcing_terms ( lev, dt, vel_g);
 
-    // HACK HACK -- NO DIVIDE BY RHO HERE YET
-    // Add (-dt/rho grad p to velocity)
+    // Convert velocities to momenta
+    for (int n = 0; n < 3; n++)
+       MultiFab::Multiply(*vel_g[lev],(*ro_g[lev]),0,n,1,vel_g[lev]->nGrow());
+
+    // Add (-dt grad p to momenta)
     MultiFab::Saxpy (*vel_g[lev], -dt,  *gp[lev], 0, 0, 3, 0);
     MultiFab::Saxpy (*vel_g[lev], -dt, *gp0[lev], 0, 0, 3, 0);
- 
-    // Add pressure gradient
-    // mfix_add_grad_phi ( lev, -dt, (*p_g[lev]) );
-    // mfix_add_grad_phi ( lev, -dt, (*p0_g[lev]) );
+
+    // Convert momenta back to velocities
+    for (int n = 0; n < 3; n++)
+       MultiFab::Divide(*vel_g[lev],(*ro_g[lev]),0,n,1,vel_g[lev]->nGrow());
 
     // If doing implicit diffusion, solve here for u^*
     if (!explicit_diffusion)
