10118221845a752e71308f5fdcfd74d295a9aa95
Ann Almgren <asalmgren@lbl.gov>
2018-09-14 07:19:48 -0700

    1) Clarify print statement if fixed_dt is greater than CFL condition (only if cfl > 1) 2) abort if that's the case

--- src_cc/projection/compute_dt.cpp
+++ src_cc/projection/compute_dt.cpp
@@ -38,11 +38,14 @@ mfix_level::mfix_compute_dt(int lev, Real time, Real stop_time, int steady_state
 
     if ( fixed_dt )
     {
-        if ( dt_new < dt )
-            amrex::Print() << "WARNING: current dt does not satisfy CFL condition: "
-                           << "min dt required = " << dt_new
-                           << " <  current dt = "  << dt
+        if ( dt_new < dt && cfl > 0)
+        {
+            amrex::Print() << "WARNING: fixed dt does not satisfy CFL condition: "
+                           << " fixed dt = "  << dt
+                           << " > dt based on cfl = " << dt_new
                            << endl;
+            amrex::Abort ("Fixed dt is too large for fluid solve");
+        }
     }
     else
     {
