c8978e4f13065975da17be0024f8cd2cf2140690
Ann Almgren <asalmgren@lbl.gov>
2018-09-17 11:59:03 -0700

    Fix sign of gp0 when based on delp

--- src_cc/set_p0.f90
+++ src_cc/set_p0.f90
@@ -115,7 +115,7 @@
                pj = pj + dpodx*dx
                p0_g(i,slo(2):shi(2),slo(3):shi(3)) = scale_pressure(pj)
             enddo
-            gp0(:,:,:,1) = dpodx
+            gp0(:,:,:,1) = -dpodx
          endif
 
          if (abs(delp_y) > epsilon(zero)) then
@@ -125,7 +125,7 @@
                pj = pj + dpody*dy
                p0_g(slo(1):shi(1),j,slo(3):shi(3)) = scale_pressure(pj)
             enddo
-            gp0(:,:,:,2) = dpody
+            gp0(:,:,:,2) = -dpody
          endif
 
          if (abs(delp_z) > epsilon(zero)) then
@@ -135,7 +135,7 @@
                pj = pj + dpodz*dz
                p0_g(slo(1):shi(1),slo(2):shi(2),k) = scale_pressure(pj)
             end do
-            gp0(:,:,:,3) = dpodz
+            gp0(:,:,:,3) = -dpodz
          endif
 
       end block
