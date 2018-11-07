308a57a794fad30ea7cea6a1f64ff98e36bd7c5e
Michele Rosso <mrosso@lbl.gov>
2018-11-07 11:06:37 -0800

    Modify grid to particle interpolation.
    * Get rid of slope interpolation.
    * Always tri-linear interpolation for velocity -- This is still NOT EB aware.
    * Trilinear interpolation for grad(p) if stencil is complete, i.e. without
      any covered cell. Otherwise just use cell grad(p).

--- src_cc/des_fluid/calc_particle_beta.f90
+++ src_cc/des_fluid/calc_particle_beta.f90
@@ -1,6 +1,6 @@
 !vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
 !                                                                      !
-!  Subroutine: calc_particle_beta                                         !
+!  Subroutine: calc_particle_beta                                      !
 !                                                                      !
 !  Purpose: This routine is called before the FLUID solve.             !
 !  It calculates the source terms for the center coefficients and RHS  !
@@ -9,32 +9,25 @@
 !                                                                      !
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
 subroutine calc_particle_beta ( slo, shi, ep_g, ro_g, vel_g, mu_g,   &
-     &                          xsl, ysl,  zsl,                      &
-     &                          np, particles, x0, dx, interp_type ) &
-     &                          bind(C)
+ &                              np, particles, x0, dx ) bind(C)
 
    use amrex_fort_module,               only: rt => amrex_real
    use iso_c_binding,                   only: c_int
    use des_drag_gp_module,              only: des_drag_gp
    use particle_mod,                    only: particle_t
    use param,                           only: zero, half, one
-   use amrex_error_module,              only: amrex_abort
-   use amrex_paralleldescriptor_module, only: amrex_pd_ioprocessor
 
    implicit none
 
    ! Array bounds
-   integer(c_int), intent(in   ) :: slo(3),shi(3)
+   integer(c_int), intent(in   ) :: slo(3), shi(3)
 
    ! Array
    real(rt), intent(in   ) :: &
-        &  ep_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3)),  &
-        &  ro_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3)),  &
-        &  mu_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3)),  &
-        & vel_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),3),&
-        & xsl(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),3),  &
-        & ysl(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),3),  &
-        & zsl(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),3)
+    &  ep_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3)),  &
+    &  ro_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3)),  &
+    &  mu_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3)),  &
+    & vel_g(slo(1):shi(1),slo(2):shi(2),slo(3):shi(3),3)
 
    ! Particles
    integer(c_int),   intent(in   ) :: np
@@ -42,14 +35,11 @@ subroutine calc_particle_beta ( slo, shi, ep_g, ro_g, vel_g, mu_g,   &
 
    ! Coordinates of lower corner of domain
    real(rt),         intent(in   ) :: x0(3)  
-   
+
    ! Grid
    real(rt),         intent(in   ) :: dx(3)
 
-   ! Interpolation type
-   integer(c_int),   intent(in   ), value :: interp_type
 
- 
    ! Local variables
    !---------------------------------------------------------------------//
    ! Loop counters: Particle, fluid cell, neighbor cells
@@ -60,23 +50,6 @@ subroutine calc_particle_beta ( slo, shi, ep_g, ro_g, vel_g, mu_g,   &
    real(rt) :: sx_lo, sy_lo, sz_lo
    real(rt) :: sx_hi, sy_hi, sz_hi
    real(rt) :: wx, wy, wz
-   
-
-   if (amrex_pd_ioprocessor()) then
-      if (interp_type==0) then 
-         write(*,*) 'WARNING: No interpolation of gas velocity &
-              &for calculating particle beta'
-      else if (interp_type==1) then
-         write(*,*) 'WARNING: Standard interpolation of gas velocity &
-              &for calculating particle beta'
-      else if (interp_type==2) then
-         write(*,*) 'WARNING: Slope-interpolation of gas velocity &
-              &for calculating particle beta'
-      else
-         call amrex_abort("calc_particle_beta(): &
-              & argument 'interp_type' has wrong value. Must be set to 0,1, or 2")
-      end if
-   end if
 
    odx  = one/dx(1)
    ody  = one/dx(2)
@@ -86,68 +59,36 @@ subroutine calc_particle_beta ( slo, shi, ep_g, ro_g, vel_g, mu_g,   &
    ! Calculate the gas phase forces acting on each particle.
    do p = 1, np
 
-      select case ( interp_type )
 
-      case (0)  ! No interpolation
+      ! Thir-linear interpolation
+      ! By adding half we always pick the upper cell in the
+      ! stencil
+      lx = (particles(p) % pos(1) - x0(1))*odx + half
+      ly = (particles(p) % pos(2) - x0(2))*ody + half
+      lz = (particles(p) % pos(3) - x0(3))*odz + half
 
-         i  = floor((particles(p) % pos(1) - x0(1))*odx)
-         j  = floor((particles(p) % pos(2) - x0(2))*ody)
-         k  = floor((particles(p) % pos(3) - x0(3))*odz)
+      i = floor(lx)
+      j = floor(ly)
+      k = floor(lz)
 
-         velfp(:)  = vel_g(i,j,k,:)
+      sx_hi = lx - i;  sx_lo = one - sx_hi
+      sy_hi = ly - j;  sy_lo = one - sy_hi
+      sz_hi = lz - k;  sz_lo = one - sz_hi         
 
-      case (1)  ! Standard interpolation
+      velfp(1:3) = sx_lo*sy_lo*sz_lo*vel_g(i-1, j-1, k-1,1:3) + &
+       &       sx_lo*sy_lo*sz_hi*vel_g(i-1, j-1, k  ,1:3) + &
+       &       sx_lo*sy_hi*sz_lo*vel_g(i-1, j  , k-1,1:3) + &
+       &       sx_lo*sy_hi*sz_hi*vel_g(i-1, j  , k  ,1:3) + &
+       &       sx_hi*sy_lo*sz_lo*vel_g(i  , j-1, k-1,1:3) + &
+       &       sx_hi*sy_lo*sz_hi*vel_g(i  , j-1, k  ,1:3) + &
+       &       sx_hi*sy_hi*sz_lo*vel_g(i  , j  , k-1,1:3) + &
+       &       sx_hi*sy_hi*sz_hi*vel_g(i  , j  , k  ,1:3)               
 
-         ! By adding half we always pick the upper cell in the
-         ! stencil
-         lx = (particles(p) % pos(1) - x0(1))*odx + half
-         ly = (particles(p) % pos(2) - x0(2))*ody + half
-         lz = (particles(p) % pos(3) - x0(3))*odz + half
-
-         i = floor(lx)
-         j = floor(ly)
-         k = floor(lz)
-
-         sx_hi = lx - i;  sx_lo = one - sx_hi
-         sy_hi = ly - j;  sy_lo = one - sy_hi
-         sz_hi = lz - k;  sz_lo = one - sz_hi         
-         
-         velfp(1:3) = sx_lo*sy_lo*sz_lo*vel_g(i-1, j-1, k-1,1:3) + &
-              &       sx_lo*sy_lo*sz_hi*vel_g(i-1, j-1, k  ,1:3) + &
-              &       sx_lo*sy_hi*sz_lo*vel_g(i-1, j  , k-1,1:3) + &
-              &       sx_lo*sy_hi*sz_hi*vel_g(i-1, j  , k  ,1:3) + &
-              &       sx_hi*sy_lo*sz_lo*vel_g(i  , j-1, k-1,1:3) + &
-              &       sx_hi*sy_lo*sz_hi*vel_g(i  , j-1, k  ,1:3) + &
-              &       sx_hi*sy_hi*sz_lo*vel_g(i  , j  , k-1,1:3) + &
-              &       sx_hi*sy_hi*sz_hi*vel_g(i  , j  , k  ,1:3)
-            
-      case(2)  ! Slope interpolation
-
-         lx = (particles(p) % pos(1) - x0(1))*odx 
-         ly = (particles(p) % pos(2) - x0(2))*ody 
-         lz = (particles(p) % pos(3) - x0(3))*odz 
-
-         i = floor(lx)
-         j = floor(ly)
-         k = floor(lz)
-
-         wx = lx - i - half
-         wy = ly - j - half
-         wz = lz - k - half
-
-         velfp(1:3) = vel_g(i,j,k,1:3)    &
-              &     + xsl(i,j,k,1:3)*wx &
-              &     + ysl(i,j,k,1:3)*wy &
-              &     + zsl(i,j,k,1:3)*wz
-
-      end select
-     
-      
       ! Calculate drag coefficient, beta
       call des_drag_gp(slo, shi, p, particles(p) % vel, velfp, &
-           ep_g(i,j,k), ro_g, mu_g, beta, i, j, k,             &
-           particles(p) % radius,  particles(p) % volume,      &
-           particles(p) % density, particles(p) % phase )
+       ep_g(i,j,k), ro_g, mu_g, beta, i, j, k,             &
+       particles(p) % radius,  particles(p) % volume,      &
+       particles(p) % density, particles(p) % phase )
 
       particles(p) % drag(1) = beta
 
--- src_cc/mfix_drag.cpp
+++ src_cc/mfix_drag.cpp
@@ -1,8 +1,8 @@
 #include <AMReX_ParmParse.H>
-
 #include <mfix_F.H>
 #include <mfix_eb_F.H>
 #include <mfix.H>
+#include <mfix_des_F.H>
 #include <AMReX_BC_TYPES.H>
 #include <AMReX_Box.H>
 #include <AMReX_EBMultiFabUtil.H>
@@ -21,15 +21,7 @@ void mfix::mfix_calc_drag_fluid(Real time)
 
        if (OnSameGrids)
        {
-       // ************************************************************
-       // First create the beta of individual particles
-       // ************************************************************
-           if (m_beta_interp_type == 2)
-               mfix_compute_velocity_slopes( lev, vel_g );
 
-           // This will overwrite user's choice
-           int beta_interp_type = 1;
-           
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
@@ -43,11 +35,8 @@ void mfix::mfix_calc_drag_fluid(Real time)
                sbx.loVect(), sbx.hiVect(),
                (*ep_g[lev])[pti].dataPtr() , (*ro_g[lev])[pti].dataPtr(),
                (*vel_g[lev])[pti].dataPtr(), (*mu_g[lev])[pti].dataPtr(),
-               (*xslopes[lev])[pti].dataPtr(),
-               (*yslopes[lev])[pti].dataPtr(),
-               (*zslopes[lev])[pti].dataPtr(),
-               &np, particles.data(), geom[lev].ProbLo(), geom[lev].CellSize(),
-               beta_interp_type );
+               &np, particles.data(),
+               geom[lev].ProbLo(), geom[lev].CellSize());
        }
 
        // ******************************************************************************
@@ -192,16 +181,9 @@ mfix::mfix_calc_drag_particle(Real time)
           // HACK -- NOTE WE ARE CALLING THIS ON ALL LEVELS BUT ONLY NEED IT ON ONE LEVEL
           int extrap_dir_bcs = 1;
           mfix_set_velocity_bcs(time, extrap_dir_bcs);
-
           gp_tmp.FillBoundary(geom[lev].periodicity());
 
-
-          if (m_drag_interp_type == 2)
-             mfix_compute_velocity_slopes( lev, vel_g );
-
-
-          // This will overwrite user selection
-          int drag_interp_type = 1;
+          
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
@@ -209,16 +191,38 @@ mfix::mfix_calc_drag_particle(Real time)
           {
               auto& particles = pti.GetArrayOfStructs();
               const int np = particles.size();
-   
-              calc_drag_particle( BL_TO_FORTRAN_ANYD(       gp_tmp[pti]),
-                                  BL_TO_FORTRAN_ANYD((  *gp0[lev])[pti]),
-                                  BL_TO_FORTRAN_ANYD((*vel_g[lev])[pti]),
-                                  (*xslopes[lev])[pti].dataPtr(),
-                                  (*yslopes[lev])[pti].dataPtr(),
-                                  BL_TO_FORTRAN_ANYD((*zslopes[lev])[pti]),
-                                  &np, particles.data(),
-                                  geom[lev].CellSize(), geom[lev].ProbLo(),
-                                  drag_interp_type);
+              Box bx = pti.tilebox ();
+
+              // this is to check efficiently if this tile contains any eb stuff
+              const EBFArrayBox&  vel_fab = static_cast<EBFArrayBox const&>((*vel_g[lev])[pti]);
+              const EBCellFlagFab&  flags = vel_fab.getEBCellFlagFab();
+
+              if (flags.getType(amrex::grow(bx,0)) == FabType::covered)
+              {
+                  // Do nothing
+              }
+              else
+              {
+                  if (flags.getType(amrex::grow(bx,1)) == FabType::regular)
+                  {
+                      calc_drag_particle( BL_TO_FORTRAN_ANYD(       gp_tmp[pti]),
+                                          BL_TO_FORTRAN_ANYD((  *gp0[lev])[pti]),
+                                          BL_TO_FORTRAN_ANYD((*vel_g[lev])[pti]),
+                                          &np, particles.data(),
+                                          geom[lev].CellSize(), geom[lev].ProbLo());
+                  }
+                  else
+                  {
+                      calc_drag_particle_eb( BL_TO_FORTRAN_ANYD(       gp_tmp[pti]),
+                                             BL_TO_FORTRAN_ANYD((  *gp0[lev])[pti]),
+                                             BL_TO_FORTRAN_ANYD((*vel_g[lev])[pti]),
+                                             BL_TO_FORTRAN_ANYD(flags),
+                                             &np, particles.data(),
+                                             geom[lev].CellSize(), geom[lev].ProbLo());
+
+                  }
+              }
+                  
           }
 
           // Reset velocity Dirichlet bc's to face values
