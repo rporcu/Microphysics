6f29fb0dcfaecbe724092ac4c8111c753e23cecd
Ann Almgren <asalmgren@lbl.gov>
2018-12-28 10:25:37 -0800

    Fix writeJobInfo in both mfixIO.cpp

--- src_staggered/io/mfixIO.cpp
+++ src_staggered/io/mfixIO.cpp
@@ -569,102 +569,86 @@ void mfix::WriteJobInfo (const std::string& dir) const
 {
     if (ParallelDescriptor::IOProcessor())
     {
-  // job_info file with details about the run
-  std::ofstream jobInfoFile;
-  std::string FullPathJobInfoFile = dir;
-  std::string PrettyLine = "===============================================================================\n";
+       // job_info file with details about the run
+       std::ofstream jobInfoFile;
+       std::string FullPathJobInfoFile = dir;
+       std::string PrettyLine = "===============================================================================\n";
 
-  FullPathJobInfoFile += "/Mfix_job_info";
-  jobInfoFile.open(FullPathJobInfoFile.c_str(), std::ios::out);
+       FullPathJobInfoFile += "/Mfix_job_info";
+       jobInfoFile.open(FullPathJobInfoFile.c_str(), std::ios::out);
 
-  // job information
-  jobInfoFile << PrettyLine;
-  jobInfoFile << " MFIx Job Information\n";
-  jobInfoFile << PrettyLine;
+       // job information
+       jobInfoFile << PrettyLine;
+       jobInfoFile << " MFIx Job Information\n";
+       jobInfoFile << PrettyLine;
 
-  jobInfoFile << "number of MPI processes: " << ParallelDescriptor::NProcs() << "\n";
+       jobInfoFile << "number of MPI processes: " << ParallelDescriptor::NProcs() << "\n";
 #ifdef _OPENMP
-  jobInfoFile << "number of threads:       " << omp_get_max_threads() << "\n";
+       jobInfoFile << "number of threads:       " << omp_get_max_threads() << "\n";
 #endif
 
-  jobInfoFile << "\n\n";
+       jobInfoFile << "\n\n";
 
         // build information
-  jobInfoFile << PrettyLine;
-  jobInfoFile << " Build Information\n";
-  jobInfoFile << PrettyLine;
+       jobInfoFile << PrettyLine;
+       jobInfoFile << " Build Information\n";
+       jobInfoFile << PrettyLine;
 
-  jobInfoFile << "build date:    " << buildInfoGetBuildDate() << "\n";
-  jobInfoFile << "build machine: " << buildInfoGetBuildMachine() << "\n";
-  jobInfoFile << "build dir:     " << buildInfoGetBuildDir() << "\n";
-  jobInfoFile << "AMReX dir:     " << buildInfoGetAMReXDir() << "\n";
+       jobInfoFile << "build date:    " << buildInfoGetBuildDate() << "\n";
+       jobInfoFile << "build machine: " << buildInfoGetBuildMachine() << "\n";
+       jobInfoFile << "build dir:     " << buildInfoGetBuildDir() << "\n";
+       jobInfoFile << "AMReX dir:     " << buildInfoGetAMReXDir() << "\n";
 
-  jobInfoFile << "\n";
+       jobInfoFile << "\n";
 
-  jobInfoFile << "COMP:          " << buildInfoGetComp() << "\n";
-  jobInfoFile << "COMP version:  " << buildInfoGetCompVersion() << "\n";
-  jobInfoFile << "FCOMP:         " << buildInfoGetFcomp() << "\n";
-  jobInfoFile << "FCOMP version: " << buildInfoGetFcompVersion() << "\n";
+       jobInfoFile << "COMP:          " << buildInfoGetComp() << "\n";
+       jobInfoFile << "COMP version:  " << buildInfoGetCompVersion() << "\n";
+       jobInfoFile << "FCOMP:         " << buildInfoGetFcomp() << "\n";
+       jobInfoFile << "FCOMP version: " << buildInfoGetFcompVersion() << "\n";
 
-  jobInfoFile << "\n";
+       jobInfoFile << "\n";
 
-  const char* githash1 = buildInfoGetGitHash(1);
-  const char* githash2 = buildInfoGetGitHash(2);
-  if (strlen(githash1) > 0)
-  {
-      jobInfoFile << "MFIX git hash: " << githash1 << "\n";
-  }
-  if (strlen(githash2) > 0)
-  {
-      jobInfoFile << "AMReX git hash: " << githash2 << "\n";
-  }
+       const char* githash1 = buildInfoGetGitHash(1);
+       const char* githash2 = buildInfoGetGitHash(2);
+       if (strlen(githash1) > 0)
+       {
+           jobInfoFile << "MFIX git hash: " << githash1 << "\n";
+       }
+       if (strlen(githash2) > 0)
+       {
+           jobInfoFile << "AMReX git hash: " << githash2 << "\n";
+       }
 
-  jobInfoFile << "\n\n";
+       jobInfoFile << "\n\n";
 
-  // grid information
+       // grid information
         jobInfoFile << PrettyLine;
         jobInfoFile << " Grid Information\n";
         jobInfoFile << PrettyLine;
 
         for (int i = 0; i <= finest_level; i++)
-  {
+        {
             jobInfoFile << " level: " << i << "\n";
             jobInfoFile << "   number of boxes = " << grids[i].size() << "\n";
             jobInfoFile << "   maximum zones   = ";
             for (int n = 0; n < BL_SPACEDIM; n++)
-      {
+            {
                 jobInfoFile << geom[i].Domain().length(n) << " ";
-      }
+            }
             jobInfoFile << "\n\n";
-  }
-
-
-  // Add here the info on BCs
-        // jobInfoFile << " Boundary conditions\n";
-
-        // jobInfoFile << "   -x: " << "interior" << "\n";
-        // jobInfoFile << "   +x: " << "interior" << "\n";
-        // if (BL_SPACEDIM >= 2) {
-  //     jobInfoFile << "   -y: " << "interior" << "\n";
-  //     jobInfoFile << "   +y: " << "interior" << "\n";
-        // }
-        // if (BL_SPACEDIM == 3) {
-  //     jobInfoFile << "   -z: " << "interior" << "\n";
-  //     jobInfoFile << "   +z: " << "interior" << "\n";
-        // }
+        }
 
         jobInfoFile << "\n\n";
 
+        // runtime parameters
+        jobInfoFile << PrettyLine;
+        jobInfoFile << " Inputs File Parameters\n";
+        jobInfoFile << PrettyLine;
 
-  // runtime parameters
-  jobInfoFile << PrettyLine;
-  jobInfoFile << " Inputs File Parameters\n";
-  jobInfoFile << PrettyLine;
+        // ParmParse::dumpTable(jobInfoFile, true);
 
-  ParmParse::dumpTable(jobInfoFile, true);
-
-  jobInfoFile.close();
-    }
+        jobInfoFile.close();
+  }
 }
 
 
