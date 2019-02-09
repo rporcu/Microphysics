457973d7a5da5551b179514add52baa568dfe610
Ann Almgren <asalmgren@lbl.gov>
2019-02-08 23:02:54 -0800

    Update AMReX version to get bug fix

--- tools/CMake/MFIX_Superbuild.cmake
+++ tools/CMake/MFIX_Superbuild.cmake
@@ -26,7 +26,7 @@ enable_language (Fortran)
 # AMReX Git variables
 set (AMREX_GIT_REPO "https://github.com/AMReX-Codes/amrex.git" )
 set (AMREX_GIT_COMMIT_MASTER   121d9c0bd059567a508de0d891e4e1ba98c4e8d3 )
-set (AMREX_GIT_COMMIT_DEVELOP  2fa070459ebdc27aa9af0e962889469bd765 )
+set (AMREX_GIT_COMMIT_DEVELOP  4eb4e7a25050ca83f02e551fcb9b8a591834395 )
 
 set (AMREX_GIT_TAG)  # The commit id or branch to download
 
