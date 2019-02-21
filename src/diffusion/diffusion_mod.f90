264d6ec2576ec0932aff8cff5f84db66b78467ae
Ann Almgren <asalmgren@lbl.gov>
2019-02-20 19:43:22 -0800

    Revert "1) restore "ignore_" in diffusion_mod.f90"
    This reverts commit 34fef2e2a67ed4c24cd24fdee30dd23c026d7a3a.

--- src/diffusion/diffusion_mod.f90
+++ src/diffusion/diffusion_mod.f90
@@ -574,7 +574,8 @@ contains
 
                      vel(:lo(1)-1,j,k,n) = 2.d0*vel_in(lo(1)-1,j,k,n) - vel_in(lo(1),j,k,n)
 
-                  else if ( bc_ilo_type(j,k,1) == pout_) then
+                  else if ( bc_ilo_type(j,k,1) == POUT_  .or. &
+                            bc_ilo_type(j,k,1) == IGNORE_ )  then
 
                      vel(:lo(1)-1,j,k,n) = vel_in(lo(1),j,k,n)
 
@@ -596,7 +597,8 @@ contains
 
                      vel(hi(1)+1:,j,k,n) = 2.d0*vel_in(hi(1)+1,j,k,n) - vel_in(hi(1),j,k,n)
 
-                  else if ( bc_ihi_type(j,k,1) == pout_) then
+                  else if ( bc_ihi_type(j,k,1) == POUT_  .or. &
+                            bc_ihi_type(j,k,1) == IGNORE_ )  then
 
                      vel(hi(1)+1:,j,k,n) = vel_in(hi(1),j,k,n)
 
@@ -618,7 +620,8 @@ contains
                      
                      vel(i,:lo(2)-1,k,n) = 2.d0*vel_in(i,lo(2)-1,k,n) - vel_in(i,lo(2),k,n)
 
-                  else if ( bc_jlo_type(i,k,1) == pout_) then
+                  else if ( bc_jlo_type(i,k,1) == POUT_  .or. &
+                            bc_jlo_type(i,k,1) == IGNORE_ )  then
                      
                      vel(i,:lo(2)-1,k,n) = vel_in(i,lo(2),k,n)
 
@@ -640,7 +643,8 @@ contains
 
                      vel(i,hi(2)+1:,k,n) = 2.d0*vel_in(i,hi(2)+1,k,n) - vel_in(i,hi(2),k,n)
 
-                  else if ( bc_jhi_type(i,k,1) == pout_) then
+                  else if ( bc_jhi_type(i,k,1) == POUT_  .or. &
+                            bc_jhi_type(i,k,1) == IGNORE_ )  then
 
                      vel(i,hi(2)+1:,k,n) = vel_in(i,hi(2),k,n)
 
@@ -663,7 +667,8 @@ contains
 
                      vel(i,j,:lo(3)-1,n) = 2.d0*vel_in(i,j,lo(3)-1,n) - vel_in(i,j,lo(3),n)
 
-                  else if ( bc_klo_type(i,j,1) == pout_) then
+                  else if ( bc_klo_type(i,j,1) == POUT_  .or. &
+                            bc_klo_type(i,j,1) == IGNORE_ )  then
 
                      vel(i,j,:lo(3)-1,n) = vel_in(i,j,lo(3),n)
 
@@ -685,7 +690,8 @@ contains
 
                      vel(i,j,hi(3)+1:,n) = 2.d0*vel_in(i,j,hi(3)+1,n) - vel_in(i,j,hi(3),n)
 
-                  else if ( bc_khi_type(i,j,1) == pout_) then
+                  else if ( bc_khi_type(i,j,1) == POUT_  .or. &
+                            bc_khi_type(i,j,1) == IGNORE_ )  then
 
                      vel(i,j,hi(3)+1:,n) = vel_in(i,j,hi(3),n)
 
