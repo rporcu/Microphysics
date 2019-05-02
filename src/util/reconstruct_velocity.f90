7748d86777c6af33558b91771b5605b7605f8148
Ann Almgren <asalmgren@lbl.gov>
2019-05-02 12:10:19 -0700

    Use my_huge instead

--- src/util/reconstruct_velocity.f90
+++ src/util/reconstruct_velocity.f90
@@ -17,7 +17,7 @@ subroutine reconstruct_velocity ( vel_out, volo, vohi,       &
    use amrex_fort_module,       only: rt => amrex_real
    use iso_c_binding,           only: c_int
    use particle_mod,            only: particle_t
-   use param,                   only: half, one, two
+   use param,                   only: half, one, two, my_huge
    use amrex_ebcellflag_module, only: is_covered_cell
    use amrex_eb_levelset_module,only: amrex_eb_interp_levelset, &
     &                                 amrex_eb_normal_levelset
@@ -141,7 +141,7 @@ subroutine reconstruct_velocity ( vel_out, volo, vohi,       &
                vel_out(i,j,k,:) = vel_i * phi_cc / phi_i
                
                else 
-                  vel_out(i,j,k,:) = 1.e49
+                  vel_out(i,j,k,:) = my_huge
                end if
 
             end if
