feaf8e463f9e4da155c49e505ac73941d51aa8de
Michele Rosso <mrosso@lbl.gov>
2019-06-20 12:22:46 -0700

    Bug fix in conv redistribution

--- src/util/mfix_divop.cpp
+++ src/util/mfix_divop.cpp
@@ -456,7 +456,7 @@ compute_divop(Box& bx,
     // Step 1: compute conservative divergence on stencil (lo-2,hi-2)
     //
 
-    AMREX_HOST_DEVICE_FOR_3D(bx, i, j, k,
+    AMREX_HOST_DEVICE_FOR_3D(grown2_bx, i, j, k,
     {
       if(flags(i,j,k).isCovered())
       {
