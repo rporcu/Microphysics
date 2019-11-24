9f89f7c3c09ddbb88dfcaea50f451028b50acdce
Ann Almgren <asalmgren@lbl.gov>
2019-11-24 07:47:32 -0800

    Now mfix_set_eps_bcs works like mfix_set_scalar_bcs used  to by using the value of m_ep_bcs_g on the face to extrapolate to the outside cell center

--- src/util/mfix_set_eps_bcs.cpp
+++ src/util/mfix_set_eps_bcs.cpp
@@ -124,15 +124,20 @@ mfix::set_eps_bcs(const int lev,
   const int pinf = bc_list.get_pinf();
   const int pout = bc_list.get_pout();
 
+  amrex::Real* p_bc_ep_g = m_bc_ep_g.data();
+
   if (nlft > 0)
   {
     amrex::ParallelFor(bx_yz_lo_3D, 
-      [bct_ilo,dom_lo,minf,pinf,pout,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      [bct_ilo,dom_lo,minf,pinf,pout,p_bc_ep_g,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
       const int bct = bct_ilo(dom_lo[0]-1,j,k,0);
+      const int bcv = bct_ilo(dom_lo[0]-1,j,k,1);
 
-      if((bct == pinf) or (bct == pout) or (bct == minf))
+      if((bct == pinf) or (bct == pout))
         eps(i,j,k) = eps(dom_lo[0],j,k);
+      else if (bct == minf)
+        eps(i,j,k) = p_bc_ep_g[bcv];
     });
 
     if(*extrap_dir_bcs > 0)
@@ -151,12 +156,15 @@ mfix::set_eps_bcs(const int lev,
   if (nrgt > 0)
   {
     amrex::ParallelFor(bx_yz_hi_3D, 
-      [bct_ihi,dom_hi,minf,pinf,pout,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      [bct_ihi,dom_hi,minf,pinf,pout,p_bc_ep_g,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
       const int bct = bct_ihi(dom_hi[0]+1,j,k,0);
+      const int bcv = bct_ihi(dom_hi[0]+1,j,k,1);
 
-      if((bct == pinf) or (bct == pout) or (bct == minf))
+      if((bct == pinf) or (bct == pout))
         eps(i,j,k) = eps(dom_hi[0],j,k);
+      else if (bct == minf)
+        eps(i,j,k) = p_bc_ep_g[bcv];
     });
 
     if(*extrap_dir_bcs > 0)
@@ -175,12 +183,15 @@ mfix::set_eps_bcs(const int lev,
   if (nbot > 0)
   {
     amrex::ParallelFor(bx_xz_lo_3D, 
-      [bct_jlo,dom_lo,minf,pinf,pout,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      [bct_jlo,dom_lo,minf,pinf,pout,p_bc_ep_g,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
       const int bct = bct_jlo(i,dom_lo[1]-1,k,0);
+      const int bcv = bct_jlo(i,dom_lo[1]-1,k,1);
 
-      if((bct == pinf) or (bct == pout) or (bct == minf))
+      if((bct == pinf) or (bct == pout))
         eps(i,j,k) = eps(i,dom_lo[1],k);
+      else if (bct == minf)
+        eps(i,j,k) = p_bc_ep_g[bcv];
     });
 
     if(*extrap_dir_bcs > 0)
@@ -200,12 +211,15 @@ mfix::set_eps_bcs(const int lev,
   if (ntop > 0)
   {
     amrex::ParallelFor(bx_xz_hi_3D, 
-      [bct_jhi,dom_hi,minf,pinf,pout,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      [bct_jhi,dom_hi,minf,pinf,pout,p_bc_ep_g,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
       const int bct = bct_jhi(i,dom_hi[1]+1,k,0);
+      const int bcv = bct_jhi(i,dom_hi[1]+1,k,1);
 
-      if((bct == pinf) or (bct == pout) or (bct == minf))
+      if((bct == pinf) or (bct == pout))
         eps(i,j,k) = eps(i,dom_hi[1],k);
+      else if (bct == minf)
+        eps(i,j,k) = p_bc_ep_g[bcv];
     });
 
     if(*extrap_dir_bcs > 0)
@@ -225,12 +239,15 @@ mfix::set_eps_bcs(const int lev,
   if (ndwn > 0)
   {
     amrex::ParallelFor(bx_xy_lo_3D, 
-      [bct_klo,dom_lo,minf,pinf,pout,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      [bct_klo,dom_lo,minf,pinf,pout,p_bc_ep_g,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
       const int bct = bct_klo(i,j,dom_lo[2]-1,0);
+      const int bcv = bct_klo(i,j,dom_lo[2]-1,1);
 
-      if((bct == pinf) or (bct == pout) or (bct == minf))
+      if((bct == pinf) or (bct == pout))
         eps(i,j,k) = eps(i,j,dom_lo[2]);
+      else if (bct == minf)
+        eps(i,j,k) = p_bc_ep_g[bcv];
     });
 
     if(*extrap_dir_bcs > 0)
@@ -250,12 +267,15 @@ mfix::set_eps_bcs(const int lev,
   if (nup > 0)
   {
     amrex::ParallelFor(bx_xy_hi_3D, 
-      [bct_khi,dom_hi,minf,pinf,pout,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+      [bct_khi,dom_hi,minf,pinf,pout,p_bc_ep_g,eps] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
       const int bct = bct_khi(i,j,dom_hi[2]+1,0);
+      const int bcv = bct_khi(i,j,dom_hi[2]+1,1);
 
-      if((bct == pinf) or (bct == pout) or (bct == minf))
+      if((bct == pinf) or (bct == pout))
         eps(i,j,k) = eps(i,j,dom_hi[2]);
+      else if (bct == minf)
+        eps(i,j,k) = p_bc_ep_g[bcv];
     });
 
     if(*extrap_dir_bcs > 0)
