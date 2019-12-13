e95dd34b7952cdbf07346618e698e5a7a7ccf067
Jordan Musser <jordan.musser@netl.doe.gov>
2019-12-13 12:24:57 -0500

    Set the density BC
    This is needed to avoid "erroneous arithmetic operation" errors
    when built with DEBUG flags. I'm not certain that we need this
    call "right here." It is likely that we could do this or something
    similar after the arrays are made and/or regridded.

--- src/mfix_advance.cpp
+++ src/mfix_advance.cpp
@@ -39,7 +39,7 @@ mfix::EvolveFluid (int nstep, Real& dt,  Real& time, Real stop_time, Real coupli
 
     // Fill ghost nodes and reimpose boundary conditions
     //mfix_set_velocity_bcs(time, vel_g, 0);
-    //mfix_set_density_bcs(time, ro_g);
+    mfix_set_density_bcs(time, ro_g);
     //mfix_set_scalar_bcs(time, trac, mu_g);
 
     //
