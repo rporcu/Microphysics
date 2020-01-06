71d09d5a77df764dc35deb6d6ec44f8faabfc5a1
William Fullmer <william.fullmer@netl.doe.gov>
2020-01-06 16:56:09 -0500

    tutorials/wdf: start HDPE dir, add draft inputs of Udf rig

---
+++ tutorials/wdf/netl_hdpe/udf_rig/usr1.f90
@@ -0,0 +1,28 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Purpose: This routine is called from the time loop and is           !
+!           user-definable.  The user may insert code in this routine  !
+!           or call appropriate user defined subroutines.  This        !
+!           can be used for setting or checking errors in quantities   !
+!           that vary with time.  This routine is not called from an   !
+!           IJK loop, hence all indices are undefined.                 !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+subroutine usr1(time)
+
+  use amrex_fort_module, only : rt => amrex_real
+  use iso_c_binding , only: c_int, c_char
+  use bc, only: bc_u_g
+
+  implicit none
+
+  real(rt),   intent(in ) :: time
+
+!
+! umf bc: uncomment to measure U_mf 
+!  
+  bc_u_g(1) = max(1.0d-15, 0.25d0 - 0.01*time)
+
+  return
+
+end subroutine usr1
