6f2a8a5fa06543c57b69d25fe4db27b4361d4ed3
Roberto Porcu <robertoporcu@lbl.gov>
2020-01-15 17:32:04 -0800

    calc_cell.f90 converted to cpp

--- src/setup/set_bc_type.f90
+++ src/setup/set_bc_type.f90
@@ -30,8 +30,6 @@ module set_bc_type_module
 
       use param, only: dim_bc
       use param, only: equal, half
-      use calc_cell_module, only: calc_cell_bc_flow
-      use calc_cell_module, only: calc_cell_bc_wall
 
       implicit none
 
