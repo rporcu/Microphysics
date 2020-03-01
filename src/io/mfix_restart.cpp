eeb5b9dbe4c859287ba9c824a44da309f8dd999b
Ann Almgren <asalmgren@lbl.gov>
2020-03-01 10:09:09 -0800

    Don't use ghost cells of original MF to fill new MF on regrid

--- src/io/mfix_restart.cpp
+++ src/io/mfix_restart.cpp
@@ -219,9 +219,6 @@ mfix::Restart (std::string& restart_file, int *nstep, Real *dt, Real *time,
               }
           }
 
-          (*m_leveldata[lev]->vel_g).FillBoundary(geom[lev].periodicity());
-          (*m_leveldata[lev]->gp   ).FillBoundary(geom[lev].periodicity());
-
        // Read scalar variables
        for (int i = 0; i < chkscalarVars.size(); i++ )
        {
@@ -264,7 +261,6 @@ mfix::Restart (std::string& restart_file, int *nstep, Real *dt, Real *time,
                   (**(chkscalarVars[i][lev]))[ib].copy(single_fab, single_fab.box(), 0, mfi.validbox(), 0, 1);
               }
           }
-          (**(chkscalarVars[i][lev])).FillBoundary(geom[lev].periodicity());
         }
        }
        amrex::Print() << "  Finished reading fluid data" << std::endl;
@@ -365,6 +361,8 @@ mfix::Restart (std::string& restart_file, int *nstep, Real *dt, Real *time,
           // Fill the bc's just in case
           m_leveldata[lev]->vel_g->FillBoundary(geom[lev].periodicity());
           m_leveldata[lev]->vel_go->FillBoundary(geom[lev].periodicity());
+
+          m_leveldata[lev]->gp->FillBoundary(geom[lev].periodicity());
         }
     }
 
