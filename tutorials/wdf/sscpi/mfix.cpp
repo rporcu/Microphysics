348e4384f8477538c7aa1d7adfaba423116e9763
William Fullmer <william.fullmer@netl.doe.gov>
2020-03-10 16:45:48 -0400

    tutorials/wdf: update inputs for mueller and sscpi for new BC format

---
+++ tutorials/wdf/sscpi/mfix.cpp
@@ -0,0 +1,409 @@
+#include <mfix_F.H>
+#include <mfix_eb_F.H>
+#include <mfix.H>
+#include <param_mod_F.H>
+
+#include <MFIX_BC_Parms.H> //wdf
+#include <MFIX_BcList.H>   //wdf
+
+#include <AMReX_BC_TYPES.H>
+#include <AMReX_Box.H>
+
+#include <MFIX_FLUID_Parms.H>
+
+std::string      mfix::particle_init_type   = "AsciiFile";
+std::string      mfix::load_balance_type    = "KnapSack";
+std::string      mfix::knapsack_weight_type = "RunTimeCosts";
+int              mfix::load_balance_fluid   = 1;
+int              mfix::knapsack_nmax        = 128;
+DragType         mfix::m_drag_type          = DragType::Invalid;
+DepositionScheme mfix::m_deposition_scheme;
+amrex::Real      mfix::tcoll_ratio          = 50.;
+amrex::Real      mfix::m_deposition_diffusion_coeff = -1.0;
+amrex::Real      mfix::m_deposition_scale_factor = 1.0;
+amrex::Real      mfix::m_max_solids_volume_fraction = 0.64356;
+
+int mfix::nlev  = 1;
+int mfix::ntrac = 1;
+
+int  mfix::plot_int        = -1;
+Real mfix::plot_per_approx = -1.;
+Real mfix::plot_per_exact  = -1.;
+
+EBSupport mfix::m_eb_support_level = EBSupport::full;
+
+RealVect mfix::gravity {0.};
+RealVect mfix::gp0     {0.};
+
+// Destructor
+mfix::~mfix ()
+{
+  if (DEM::solve)
+    delete pc;
+
+  for (int lev(0); lev < nlev; ++lev)
+  {
+    // Face-based coefficients b in MAC projection and implicit diffusion solve
+    delete bcoeff[lev][0];
+    delete bcoeff[lev][1];
+    delete bcoeff[lev][2];
+
+    // Boundary conditions types
+    delete bc_ilo[lev];
+    delete bc_ihi[lev];
+    delete bc_jlo[lev];
+    delete bc_jhi[lev];
+    delete bc_klo[lev];
+    delete bc_khi[lev];
+
+    // Level-Set Data
+    delete level_sets[lev];
+  }
+
+  //! EB factory that lives on the fluid grids
+  for (int lev(0); lev < ebfactory.size(); lev++)
+    delete ebfactory[lev];
+
+  //! EB factory that lives on the particle grids
+  for (int lev(0); lev < particle_ebfactory.size(); ++lev)
+    delete particle_ebfactory[lev];
+};
+
+// Constructor
+mfix::mfix ()
+  : m_bc_u_g(get_dim_bc()+1, 0)
+  , m_bc_v_g(get_dim_bc()+1, 0)
+  , m_bc_w_g(get_dim_bc()+1, 0)
+  , m_bc_t_g(get_dim_bc()+1, 0)
+  , m_bc_ep_g(get_dim_bc()+1, 0)
+  , m_bc_p_g(get_dim_bc()+1, 0)
+{
+    // NOTE: Geometry on all levels has just been defined in the AmrCore
+    // constructor. No valid BoxArray and DistributionMapping have been defined.
+    // But the arrays for them have been resized.
+
+    /****************************************************************************
+     *                                                                          *
+     * Set max number of levels (nlevs)                                         *
+     *                                                                          *
+     ***************************************************************************/
+
+    nlev = maxLevel() + 1;
+    amrex::Print() << "Number of levels: " << nlev << std::endl;
+
+
+    /****************************************************************************
+     *                                                                          *
+     * Initialize time steps                                                    *
+     *                                                                          *
+     ***************************************************************************/
+
+    t_old.resize(nlev,-1.e100);
+    t_new.resize(nlev,0.0);
+
+
+    /****************************************************************************
+     *                                                                          *
+     * Initialize boundary conditions (used by fill-patch)                      *
+     *                                                                          *
+     ***************************************************************************/
+
+    bcs_u.resize(3); // one for each velocity component
+    bcs_s.resize(2); // density and tracer
+    bcs_f.resize(1); // just one
+
+    //___________________________________________________________________________
+    // Boundary conditions used for level-sets
+
+    bcs_ls.resize(1);
+
+    // walls (Neumann)
+    int bc_lo[] = {FOEXTRAP, FOEXTRAP, FOEXTRAP};
+    int bc_hi[] = {FOEXTRAP, FOEXTRAP, FOEXTRAP};
+
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+        // lo-side BCs
+        if (bc_lo[idim] == BCType::int_dir  ||  // periodic uses "internal Dirichlet"
+            bc_lo[idim] == BCType::foextrap ||  // first-order extrapolation
+            bc_lo[idim] == BCType::ext_dir ) {  // external Dirichlet
+            bcs_ls[0].setLo(idim, bc_lo[idim]);
+        }
+        else {
+            amrex::Abort("Invalid level-set bc_lo");
+        }
+
+        // hi-side BCSs
+        if (bc_hi[idim] == BCType::int_dir  ||  // periodic uses "internal Dirichlet"
+            bc_hi[idim] == BCType::foextrap ||  // first-order extrapolation
+            bc_hi[idim] == BCType::ext_dir ) {  // external Dirichlet
+            bcs_ls[0].setHi(idim, bc_hi[idim]);
+        }
+        else {
+            amrex::Abort("Invalid level-set bc_hi");
+        }
+    }
+}
+
+void
+mfix::mfix_usr1_cpp (Real time) const
+{
+  mfix_usr1(&time);
+
+  const int minf_ = bc_list.get_minf(); //wdf
+  const int dim_bc = get_dim_bc();
+
+  // for(unsigned i(1); i <= dim_bc; ++i)
+  // {
+  //   m_bc_u_g[i] = get_bc_u_g(i);
+  //   m_bc_v_g[i] = get_bc_v_g(i);
+  //   m_bc_w_g[i] = get_bc_w_g(i);
+
+  //   m_bc_t_g[i] = get_bc_t_g(i);
+
+  //   m_bc_ep_g[i] = get_bc_ep_g(i);
+  // }
+
+   //wdf
+   for(unsigned bcv(0); bcv < BC::bc.size(); ++bcv) {
+      if ( FLUID::solve && BC::bc[bcv].type == minf_ ) {
+         m_bc_u_g[bcv] = 0.0;
+         //2umf: m_bc_u_g[bcv] = std::max(1.0 + time, 2.19);
+         //3umf: m_bc_u_g[bcv] = std::max(1.0 + time, 3.28);
+         //4umf: m_bc_u_g[bcv] = std::max(1.0 + time, 4.38);
+         m_bc_v_g[bcv] = 0.0;
+         m_bc_w_g[bcv] = 0.0;
+      }
+   }//wdf
+
+}
+
+void
+mfix::usr3 ()
+{
+    if (FLUID::solve)
+    {
+       for (int lev = 0; lev < nlev; lev++)
+       {
+          Real dx = geom[lev].CellSize(0);
+          Real dy = geom[lev].CellSize(1);
+          Real dz = geom[lev].CellSize(2);
+
+          // We deliberately don't tile this loop
+          for (MFIter mfi(*(m_leveldata[lev]->p_g), false); mfi.isValid(); ++mfi)
+          {
+             mfix_usr3(BL_TO_FORTRAN_ANYD((*m_leveldata[lev]->vel_g)[mfi]),
+                       BL_TO_FORTRAN_ANYD((*m_leveldata[lev]->p_g)[mfi]),
+                       &dx, &dy, &dz);
+          }
+       }
+    }
+}
+
+
+void
+mfix::avgDown (int crse_lev, const MultiFab& S_fine, MultiFab& S_crse)
+{
+    BL_PROFILE("mfix::avgDown()");
+
+    amrex::EB_average_down(S_fine, S_crse, 0, S_fine.nComp(), refRatio(crse_lev));
+}
+
+Vector< MultiFab* > mfix::get_ep_g () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->ep_g);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_ro_g () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->ro_g);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_ro_g_old () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->ro_go);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_trac () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->trac);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_trac_old () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->trac_o);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_vel_g () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->vel_g);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_vel_g_old () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->vel_go);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_mu_g () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->mu_g);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_drag () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->drag);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_xslopes_u () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->xslopes_u);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_yslopes_u () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->yslopes_u);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_zslopes_u () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->zslopes_u);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_xslopes_s () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->xslopes_s);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_yslopes_s () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->yslopes_s);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_zslopes_s () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->zslopes_s);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_diveu () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->diveu);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_mac_phi () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->mac_phi);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_u_mac () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->u_mac);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_v_mac () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->v_mac);
+  }
+  return r;
+}
+
+Vector< MultiFab* > mfix::get_w_mac () noexcept
+{
+  Vector<MultiFab*> r;
+  r.reserve(m_leveldata.size());
+  for (int lev = 0; lev < m_leveldata.size(); ++lev) {
+    r.push_back(m_leveldata[lev]->w_mac);
+  }
+  return r;
+}
