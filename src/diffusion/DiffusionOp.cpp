71b807fd648ae9b54b4a7a5812ba6fd2e43ee5d3
Ann Almgren <asalmgren@lbl.gov>
2020-04-27 08:29:51 -0700

    Change call to single_level_weighted_redistribute to reflect change in amrex

--- src/diffusion/DiffusionOp.cpp
+++ src/diffusion/DiffusionOp.cpp
@@ -201,13 +201,12 @@ void DiffusionOp::ComputeDivTau (Vector< MultiFab* >& divtau_out,
  
     for(int lev = 0; lev <= finest_level; lev++)
     {
-       amrex::single_level_weighted_redistribute(lev,
-                                                 *divtau_aux[lev],
+       amrex::single_level_weighted_redistribute(*divtau_aux[lev],
                                                  *divtau_out[lev],
                                                  *ep_in[lev],
                                                  0,
                                                  AMREX_SPACEDIM,
-                                                 geom);
+                                                 geom[lev]);
  
        // Divide by density
        for (int n = 0; n < 3; n++)
@@ -225,7 +224,7 @@ void DiffusionOp::ComputeLapS (Vector< MultiFab* >& laps_out,
                                const Vector< MultiFab* >& scal_in,
                                const Vector< MultiFab* >& ro_in,
                                const Vector< MultiFab* >& ep_in,
-                               const Vector< Real > mu_s) 
+                               const Vector< Real      >& mu_s) 
 {
     BL_PROFILE("DiffusionOp::ComputeLapS");
 
@@ -264,7 +263,7 @@ void DiffusionOp::ComputeLapS (Vector< MultiFab* >& laps_out,
  
     for(int lev = 0; lev <= finest_level; lev++)
     {
-       amrex::single_level_redistribute(lev, *laps_aux[lev], *laps_out[lev], 0, ntrac, geom);
+       amrex::single_level_redistribute(*laps_aux[lev], *laps_out[lev], 0, ntrac, geom[lev]);
     }
     
     for(int lev = 0; lev <= finest_level; lev++)
