4f48f1ebf0b6fd4854dc431850b6d0976d9fae18
Roberto Porcu <robbberto88@gmail.com>
2020-05-16 23:44:45 -0400

    Remove use of function ls_has_walls

--- src/des/MFIXParticleContainer.cpp
+++ src/des/MFIXParticleContainer.cpp
@@ -204,7 +204,9 @@ void MFIXParticleContainer::EvolveParticles (int lev,
             // Set tol to 1/2 dx
             Real tol = std::min(dx[0], std::min(dx[1], dx[2])) / 2;
 
-            has_wall = ( ls_has_walls(refined_box,(*ls_phi)[pti],tol) > 0 );
+            Real ls_min_over_box = ((*ls_phi)[pti]).min<RunOn::Gpu>(refined_box,0);
+
+            if (ls_min_over_box < tol) has_wall = true;
         }
 
         tile_has_walls[index] = has_wall;
