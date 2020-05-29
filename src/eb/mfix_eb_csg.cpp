7c3c4031a67f57f153142e49b0dc406b33c7c8b1
Deepak Rangarajan <deepak.rangarajan@netl.doe.gov>
2020-05-29 16:14:58 +0000

    Csg complement

--- src/eb/mfix_eb_csg.cpp
+++ src/eb/mfix_eb_csg.cpp
@@ -3,7 +3,12 @@
 #include <csg.hpp>
 
 void mfix::make_eb_csg(std::string geom_file) {
-  auto csg_if = csg::get_csgif_from_filename(geom_file);
+  bool is_internal_flow = true;
+  ParmParse pp("csg");
+  pp.query("internal_flow", is_internal_flow);
+  amrex::Print() << "\n Building geometry with is_internal_flow:  " << is_internal_flow << std::endl;
+
+  auto csg_if = csg::get_csgif(geom_file, is_internal_flow);
   AMREX_ALWAYS_ASSERT_WITH_MESSAGE(csg_if, "Unable to create CsgIF from geometry file");
   auto gshop = EB2::makeShop(*csg_if);
   build_eb_levels(gshop);
