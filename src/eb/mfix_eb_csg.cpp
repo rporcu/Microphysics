07f3c29b337a78046d2b1dfc1794fcbb11d629a4
Deepak Rangarajan <deepak.rangarajan@netl.doe.gov>
2020-09-24 13:00:08 +0000

    Csg scale and translate

--- src/eb/mfix_eb_csg.cpp
+++ src/eb/mfix_eb_csg.cpp
@@ -1,15 +1,30 @@
 #include <mfix.H>
+#include <AMReX_EB2_IF.H>
+#include <AMReX_EB2_IF_Scale.H>
+#include <AMReX_EB2_IF_Translation.H>
 
 #include <csg.hpp>
 
 void mfix::make_eb_csg(std::string geom_file) {
   bool is_internal_flow = true;
+  Vector<Real> scaling_factor_vec(3, 1.0);
+  Vector<Real> translation_vec(3, 0.0);
+
   ParmParse pp("csg");
   pp.query("internal_flow", is_internal_flow);
-  amrex::Print() << "\n Building geometry with is_internal_flow:  " << is_internal_flow << std::endl;
+  if(pp.queryarr("scaling_factor", scaling_factor_vec, 0, 3)) {
+    amrex::Print() << "WARNING: The implicit function magnitudes will not be scaled" << std::endl;
+  }
+  pp.queryarr("translation", translation_vec, 0, 3);
+  Array<Real,3> scaling_factor = {scaling_factor_vec[0], scaling_factor_vec[1], scaling_factor_vec[2]};
+  Array<Real,3> translation = {translation_vec[0], translation_vec[1], translation_vec[2]};
 
+  amrex::Print() << "\n Building geometry with is_internal_flow:  " << is_internal_flow << std::endl;
   auto csg_if = csg::get_csgif(geom_file, is_internal_flow);
   AMREX_ALWAYS_ASSERT_WITH_MESSAGE(csg_if, "Unable to create CsgIF from geometry file");
-  auto gshop = EB2::makeShop(*csg_if);
+
+  auto final_csg_if = EB2::translate(EB2::scale(*csg_if, scaling_factor), translation);
+  
+  auto gshop = EB2::makeShop(final_csg_if);
   build_eb_levels(gshop);
 }
--- src/eb/tests/benchmarks/05-cyl-fluidbed.cpp
+++ src/eb/tests/benchmarks/05-cyl-fluidbed.cpp
@@ -1,6 +1,8 @@
 #include "catch2/catch.hpp"
 
 #include <AMReX_EB2_IF_Cylinder.H>
+#include <AMReX_EB2_IF_Scale.H>
+#include <AMReX_EB2_IF_Translation.H>
 
 #include "main.hpp"
 
@@ -13,12 +15,13 @@ const Real Cx = 0.002;
 const Real Cy = 0.0005;
 const Real Cz = 0.0005;
 
-void check(const csg::CsgIF &my_if, const EB2::CylinderIF &their_cyl,
+template <class IFA, class IFB>
+void check(const IFA &my_if, const IFB &their_cyl,
            std::tuple<double, double, double> point) {
   auto xx = std::get<0>(point);
   auto yy = std::get<1>(point);
   auto zz = std::get<2>(point);
-  CHECK(Approx(my_if(xx, yy, zz)) == their_cyl(xx, yy, zz));
+  CHECK(Approx(my_if({xx, yy, zz})) == their_cyl({xx, yy, zz}));
 }
 
 TEST_CASE("05-cyl-fluidbed", "[benchmarks]") {
@@ -110,7 +113,7 @@ difference() {
     REQUIRE(maybe_tree);
     csg::CsgIF my_if(maybe_tree, true);
 
-    SECTION("Inside the hole") {
+    SECTION("Inside the cylinder") {
       check(my_if, their_cyl, {Cx + 0.49, Cy, Cz});
       check(my_if, their_cyl, {Cx - 0.49, Cy, Cz});
       check(my_if, their_cyl, {Cx, Cy, Cz});
@@ -121,13 +124,88 @@ difference() {
       check(my_if, their_cyl, {Cx, Cy - radius * 0.9, Cz});
     }
 
-    SECTION("Outside the hole") {
+    SECTION("Outside the cylinder") {
       check(my_if, their_cyl, {Cx + 0.51, Cy, Cz});
       check(my_if, their_cyl, {Cx - 0.51, Cy, Cz});
       check(my_if, their_cyl, {Cx, Cy + radius * 0.9, Cz - radius * 0.9});
       check(my_if, their_cyl, {Cx, Cy - radius * 0.9, Cz + radius * 0.9});
       check(my_if, their_cyl, {Cx, Cy, Cz + radius * 1.1});
       check(my_if, their_cyl, {Cx, Cy - radius * 1.1, Cz});
+    }
+  }
+
+  SECTION("Default translation and scaling") {
+    auto csg_str = std::string(R"(
+	multmatrix([[0, 0, 1, 0.002], [0, 1, 0, 0.0005], [-1, 0, 0, 0.0005], [0, 0, 0, 1]]) {
+		cylinder(h = 1.0, r1 = 0.00045, r2 = 0.00045, center = true);
+	}
+  )");
+    auto maybe_tree = csg::parse_csg(csg_str);
+    REQUIRE(maybe_tree);
+    csg::CsgIF my_if(maybe_tree, true);
+    auto my_new_if =
+        EB2::translate(EB2::scale(my_if, {1.0, 1.0, 1.0}), {0.0, 0.0, 0.0});
+
+    SECTION("Inside the cylinder") {
+      check(my_new_if, their_cyl, {Cx + 0.49, Cy, Cz});
+      check(my_new_if, their_cyl, {Cx - 0.49, Cy, Cz});
+      check(my_new_if, their_cyl, {Cx, Cy, Cz});
+
+      check(my_new_if, their_cyl, {Cx, Cy + radius * 0.7, Cz - radius * 0.7});
+      check(my_new_if, their_cyl, {Cx, Cy - radius * 0.7, Cz + radius * 0.7});
+      check(my_new_if, their_cyl, {Cx, Cy, Cz + radius * 0.9});
+      check(my_new_if, their_cyl, {Cx, Cy - radius * 0.9, Cz});
+    }
+
+    SECTION("Outside the cylinder") {
+      check(my_new_if, their_cyl, {Cx + 0.51, Cy, Cz});
+      check(my_new_if, their_cyl, {Cx - 0.51, Cy, Cz});
+      check(my_new_if, their_cyl, {Cx, Cy + radius * 0.9, Cz - radius * 0.9});
+      check(my_new_if, their_cyl, {Cx, Cy - radius * 0.9, Cz + radius * 0.9});
+      check(my_new_if, their_cyl, {Cx, Cy, Cz + radius * 1.1});
+      check(my_new_if, their_cyl, {Cx, Cy - radius * 1.1, Cz});
+    }
+  }
+
+  SECTION("Translation and scaling") {
+    EB2::CylinderIF raw_cyl(radius * 1000, height * 1000, direction,
+                            {0.0, 0.0, 0.0}, true);
+    auto their_new_cyl = EB2::translate(
+        EB2::scale(raw_cyl, {0.001, 0.001, 0.001}), {0.002, 0.0005, 0.0005});
+
+    auto csg_str = std::string(R"(
+	multmatrix([[0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 0, 1]]) {
+		cylinder(h = 1000.0, r1 = 0.45, r2 = 0.45, center = true);
+	}
+  )");
+    auto maybe_tree = csg::parse_csg(csg_str);
+    REQUIRE(maybe_tree);
+    csg::CsgIF my_if(maybe_tree, true);
+    auto my_new_if = EB2::translate(EB2::scale(my_if, {0.001, 0.001, 0.001}),
+                                    {0.002, 0.0005, 0.0005});
+
+    SECTION("Inside the cylinder") {
+      check(my_new_if, their_new_cyl, {Cx + 0.49, Cy, Cz});
+      check(my_new_if, their_new_cyl, {Cx - 0.49, Cy, Cz});
+      check(my_new_if, their_new_cyl, {Cx, Cy, Cz});
+
+      check(my_new_if, their_new_cyl,
+            {Cx, Cy + radius * 0.7, Cz - radius * 0.7});
+      check(my_new_if, their_new_cyl,
+            {Cx, Cy - radius * 0.7, Cz + radius * 0.7});
+      check(my_new_if, their_new_cyl, {Cx, Cy, Cz + radius * 0.9});
+      check(my_new_if, their_new_cyl, {Cx, Cy - radius * 0.9, Cz});
+    }
+
+    SECTION("Outside the cylinder") {
+      check(my_new_if, their_new_cyl, {Cx + 0.51, Cy, Cz});
+      check(my_new_if, their_new_cyl, {Cx - 0.51, Cy, Cz});
+      check(my_new_if, their_new_cyl,
+            {Cx, Cy + radius * 0.9, Cz - radius * 0.9});
+      check(my_new_if, their_new_cyl,
+            {Cx, Cy - radius * 0.9, Cz + radius * 0.9});
+      check(my_new_if, their_new_cyl, {Cx, Cy, Cz + radius * 1.1});
+      check(my_new_if, their_new_cyl, {Cx, Cy - radius * 1.1, Cz});
     }
   }
 }
