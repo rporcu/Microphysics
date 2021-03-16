6048868d5c40a262f3f27e92bf804f7b91385ec9
Ann Almgren <asalmgren@lbl.gov>
2021-03-16 16:17:51 -0700

    Two fixes for state redistribution:     1) use scratch array instead of modifying dUdt_in -- this was a bug     when tiling was used     2) use Atomic::Add instead state redistribution when we add to nbors      of (i,j,k) instead of to (i,j,k) -- this hopefully fixes SR on GPUs

--- src/convection/Redistribution/mfix_state_redistribute.cpp
+++ src/convection/Redistribution/mfix_state_redistribute.cpp
@@ -225,10 +225,12 @@ redistribution::state_redistribute ( Box const& bx, int ncomp, int icomp,
             {
                 const auto& slopes_eb = amrex_lim_slopes_eb(i,j,k,n,soln_hat,cent_hat,fcx,fcy,fcz,flag);
 
-                U_out(i,j,k,n+icomp) +=soln_hat(i,j,k,n);
-                                     + slopes_eb[0] * (ccent(i,j,k,0)-cent_hat(i,j,k,0))
-                                     + slopes_eb[1] * (ccent(i,j,k,1)-cent_hat(i,j,k,1))
-                                     + slopes_eb[2] * (ccent(i,j,k,2)-cent_hat(i,j,k,2));
+                Real update = soln_hat(i,j,k,n)
+                            + slopes_eb[0] * (ccent(i,j,k,0)-cent_hat(i,j,k,0))
+                            + slopes_eb[1] * (ccent(i,j,k,1)-cent_hat(i,j,k,1))
+                            + slopes_eb[2] * (ccent(i,j,k,2)-cent_hat(i,j,k,2));
+                amrex::Gpu::Atomic::Add(&U_out(i,j,k,n+icomp),update);
+
             } // n
         } // vfrac
     });
