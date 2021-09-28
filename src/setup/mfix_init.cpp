d92ebd891408afe1221756e86f0661743a5fe92b
Roberto Porcu <robbberto88@gmail.com>
2021-09-28 15:51:52 -0400

    fix a bug

--- src/setup/mfix_init.cpp
+++ src/setup/mfix_init.cpp
@@ -394,26 +394,6 @@ mfix::InitParams ()
       }
     }
 
-    // Constraint type
-    {
-      std::string constraint_type = "IncompressibleFluid";
-      pp.query("constraint_type", constraint_type);
-      constraint_type = amrex::toLower(constraint_type);
-
-      if (constraint_type.compare("incompressiblefluid") == 0) {
-        m_constraint_type = ConstraintType::IncompressibleFluid;
-      }
-      else if (constraint_type.compare("idealgasopensystem") == 0) {
-        m_constraint_type = ConstraintType::IdealGasOpenSystem;
-      }
-      else if (constraint_type.compare("idealgasclosedsystem") == 0) {
-        m_constraint_type = ConstraintType::IdealGasClosedSystem;
-      }
-      else {
-        amrex::Abort("Don't know this constraint type!");
-      }
-    }
-
     std::string deposition_scheme = "trilinear";
     pp.query("deposition_scheme", deposition_scheme);
 
@@ -442,6 +422,31 @@ mfix::InitParams ()
 
     m_deposition_diffusion_coeff = -1.;
     pp.query("deposition_diffusion_coeff", m_deposition_diffusion_coeff);
+  }
+
+  if (fluid.solve)
+  {
+    ParmParse pp("mfix");
+
+    // Constraint type
+    {
+      std::string constraint_type = "IncompressibleFluid";
+      pp.query("constraint_type", constraint_type);
+      constraint_type = amrex::toLower(constraint_type);
+
+      if (constraint_type.compare("incompressiblefluid") == 0) {
+        m_constraint_type = ConstraintType::IncompressibleFluid;
+      }
+      else if (constraint_type.compare("idealgasopensystem") == 0) {
+        m_constraint_type = ConstraintType::IdealGasOpenSystem;
+      }
+      else if (constraint_type.compare("idealgasclosedsystem") == 0) {
+        m_constraint_type = ConstraintType::IdealGasClosedSystem;
+      }
+      else {
+        amrex::Abort("Don't know this constraint type!");
+      }
+    }
   }
 
   {
