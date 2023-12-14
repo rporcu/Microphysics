1edd48878714a5bd68857dfb1b05c14ff8944d23
Roberto Porcu <robbberto88@gmail.com>
2023-12-14 17:14:48 -0500

    remove unit_test folder

--- exec/main.cpp
+++ exec/main.cpp
@@ -7,22 +7,10 @@
 using namespace amrex;
 
 #include <extern_parameters.H>
-#include <burn_cell.H>
-#include <unit_test.H>
-
-using namespace unit_test_rp;
 
 int main(int argc, char *argv[]) {
 
   amrex::Initialize(argc, argv);
-
-  std::cout << "starting the single zone burn..." << std::endl;
-
-  ParmParse ppa("amr");
-
-  init_unit_test();
-
-  burn_cell_c();
 
   amrex::Finalize();
 }
